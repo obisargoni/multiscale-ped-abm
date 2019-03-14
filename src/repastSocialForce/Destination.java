268f07bf8fd5dad1024ea195ef388419177f7f8d
Obi Thompson Sargoni <33906525+obisargoni@users.noreply.github.com>
2019-03-14 12:39:31 +0000

    gis1 Getting rid of the source and adding pedestrian agents to the context and geography directly form the context builder. This allows user to see initial positions of pedestrian agents. Previous set up would only display pedestrian agents one time step in which was confusing for testing and debugging.

--- src/repastSocialForce/Destination.java
+++ src/repastSocialForce/Destination.java
@@ -1,7 +1,16 @@
 package repastSocialForce;
 
+import java.util.ArrayList;
+
+import com.vividsolutions.jts.geom.Geometry;
+
+import repast.simphony.context.Context;
+import repast.simphony.engine.environment.RunEnvironment;
+import repast.simphony.engine.schedule.ScheduleParameters;
+import repast.simphony.engine.schedule.ScheduledMethod;
 import repast.simphony.space.continuous.ContinuousSpace;
 import repast.simphony.space.gis.Geography;
+import repast.simphony.util.ContextUtils;
 
 public class Destination {
 
@@ -10,5 +19,38 @@ public class Destination {
 	public Destination(Geography<Object> geography) {
 		this.geography = geography;
 	}
+	
+	@ScheduledMethod(start = 1, interval = 1, priority = ScheduleParameters.LAST_PRIORITY)
+	 public void removePeds() {
+	        Context<Object> context = ContextUtils.getContext(this);
+	        Geography<Object> geography = this.geography;
+	        
+	        ArrayList<Ped> PedsToRemove = new ArrayList<Ped>();
+	        
+	        // If there are no pedestrians to remove end the simulation
+	        if (context.getObjects(Ped.class).size() == 0) {
+	        	RunEnvironment.getInstance().endRun();
+	        }
+	        
+	        // Iterate over peds and remove them if they have arrive at the destination
+	        for (Object p :context.getObjects(Ped.class)) {
+	        	Ped P = (Ped) p;
+
+	        	Geometry dGeom = geography.getGeometry(P.destination);
+	        	Geometry coordP = geography.getGeometry(P);
+	        	
+	        	// If the pedestrian agent in within the bounds of the destination then remove it from the context as it has reached its destination
+	        	if (dGeom.contains(coordP)) {
+	        		//Source.removedPeds.add(P);
+	        		PedsToRemove.add(P);
+	        		break; // End the iteration since iterating over context having modified it can throw an exception
+	        	}
+	        }
+	        // Now iterate over all of the peds to remove and remove them from the context
+	        // Need to do this separately from iterating over the peds in the context since altering the context whilst iterating over it throws and exception
+	        for (Ped P : PedsToRemove) {
+	        	context.remove(P);
+	        }
+	    }
 
 }
--- src/repastSocialForce/SpaceBuilder.java
+++ src/repastSocialForce/SpaceBuilder.java
@@ -7,7 +7,9 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
+import org.apache.commons.math3.util.FastMath;
 import org.geotools.data.shapefile.ShapefileDataStore;
 import org.geotools.data.simple.SimpleFeatureIterator;
 import org.opengis.feature.simple.SimpleFeature;
@@ -16,18 +18,20 @@ import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Point;
 
 import repast.simphony.context.Context;
 import repast.simphony.context.DefaultContext;
 import repast.simphony.context.space.gis.GeographyFactoryFinder;
 import repast.simphony.dataLoader.ContextBuilder;
 import repast.simphony.engine.environment.RunEnvironment;
+import repast.simphony.engine.schedule.ScheduleParameters;
+import repast.simphony.engine.schedule.ScheduledMethod;
 import repast.simphony.gis.util.GeometryUtil;
 import repast.simphony.parameter.Parameters;
 import repast.simphony.space.gis.Geography;
 import repast.simphony.space.gis.GeographyParameters;
-import repast.simphony.space.gis.RepastCoverageFactory;
-import repast.simphony.space.gis.WritableGridCoverage2D;
+import repast.simphony.util.ContextUtils;
 
 public class SpaceBuilder extends DefaultContext<Object> implements ContextBuilder<Object> {
 	
@@ -48,11 +52,6 @@ public class SpaceBuilder extends DefaultContext<Object> implements ContextBuild
 		GeographyParameters geoParams = new GeographyParameters();
 		//geoParams.setCrs("EPSG:27700");
 		Geography<Object> geography = GeographyFactoryFinder.createGeographyFactory(null).createGeography("Geography", context, geoParams);
-	    
-		// Add raster coverage from file to the geography
-		//File osmaster_raster = new File("S:\\CASA_obits_ucfnoth\\1. PhD Work\\GIS Data\\CoventGardenWaterloo\\Clipped layers\\Masterma_1250_clipped.tif");
-		//WritableGridCoverage2D coverage = RepastCoverageFactory.createWritableCoverageFromFile(osmaster_raster, true);
-		//geography.addCoverage("osraster", coverage);
 		
 		context.add(geography);
 	    
@@ -66,23 +65,33 @@ public class SpaceBuilder extends DefaultContext<Object> implements ContextBuild
 		
 	    
 	    // A separate class is used to handle the creation of pedestrians
-	    Destination d1 = addRandomDestination(context, geography, fac, boundary, 5);
-	    Destination d2 = addRandomDestination(context, geography, fac, boundary, 5);
+	    Destination d1 = addRandomDestination(context, geography, fac, boundary, 0.01);
+	    Destination d2 = addRandomDestination(context, geography, fac, boundary, 0.01);
 	    //Destination d1 = addUserDestination(context, geography, fac, "destX1", "destY1", 5);
 	    //Destination d2 = addUserDestination(context, geography, fac, "destX2", "destY2", 5);
-	    Source flowSource1 = new Source(boundary, fac, d1, 100, Color.RED);
-	    Source flowSource2 = new Source(boundary, fac, d2, 100, Color.BLUE);
-	    context.add(flowSource1);
-	    context.add(flowSource2);
 	    
-		//ISchedule schedule = RunEnvironment.getInstance().getCurrentSchedule();
-		//ScheduleParameters params = ScheduleParameters.createAtEnd(ScheduleParameters.LAST_PRIORITY);
-		//context.add(clock);
+    	// Get the number of pedetrian agent to add to the space from the parameters
+    	Parameters params = RunEnvironment.getInstance().getParameters();
+    	int nP = (int)params.getInteger("nPeds");
+
+		// Generate random points in the area to create agents.
+		List<Coordinate> agentCoords = GeometryUtil.generateRandomPointsInPolygon(boundary, nP);
+		
+		// Create the agents from the collection of random coords.
+		Random randCoord = new Random();
+		for (Coordinate coord : agentCoords) {
+			
+			// Generate a random initial direction for the pedestrian
+    		double randBearing = randCoord.nextFloat() * FastMath.PI * 2;
+    		double[] dir = {FastMath.sin(randBearing), FastMath.cos(randBearing)};
+			
+    		Ped newPed = addPed(context, geography, fac, dir,coord, d1, Color.BLUE);
+		}
 		
 		return context;
 	}
 	
-	public Destination addRandomDestination(Context<Object> context, Geography<Object> geography, GeometryFactory gF, Geometry bndry, int destExtent) {
+	public Destination addRandomDestination(Context<Object> context, Geography<Object> geography, GeometryFactory gF, Geometry bndry, double destExtent) {
 		
 		Destination d = new Destination(geography);
 		context.add(d);
@@ -92,7 +101,7 @@ public class SpaceBuilder extends DefaultContext<Object> implements ContextBuild
 		
 		// Get the coordinate and buffer it by the extent of the destination to create a 
 		// circle that defines the destination
-		Geometry destGeom = gF.createPoint(destCoord).buffer(destExtent);
+		Geometry destGeom = gF.createPoint(destCoord);//.buffer(destExtent);
 		geography.move(d, destGeom);
 				
 		return d;
@@ -120,6 +129,20 @@ public class SpaceBuilder extends DefaultContext<Object> implements ContextBuild
 		return d;
 		
 	}
+	
+    public Ped addPed(Context context, Geography geography, GeometryFactory gF, double[] direction, Coordinate coord, Destination d, Color color) {
+        
+        // Instantiate a new pedestrian agent and add the agent to the context
+        Ped newPed = new Ped(geography, direction, d, color);
+        context.add(newPed);
+        
+        // Create a new point geometry. Move the pedestrian to this point. In doing so this 
+        // pedestrian agent becomes associated with this geometry.
+		Point geom = gF.createPoint(coord);
+		geography.move(newPed, geom);
+        	
+        return newPed;
+    }
 	
 	/*
 	 * Taken from the Geography RS example. Returns a list of SimpleFeature
@@ -156,4 +179,5 @@ public class SpaceBuilder extends DefaultContext<Object> implements ContextBuild
 		
 		return features;
 	}
+	
 }
