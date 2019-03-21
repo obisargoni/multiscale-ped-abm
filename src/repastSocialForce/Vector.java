0a16ddec940cd0c880b7a71dbf9aab742dc4bbb9
Obi Thompson Sargoni <33906525+obisargoni@users.noreply.github.com>
2019-03-20 18:29:09 -0700

    gis1 Peds now head to a single destination

--- src/repastSocialForce/Vector.java
+++ src/repastSocialForce/Vector.java
@@ -1,6 +1,17 @@
 package repastSocialForce;
 
 import org.apache.commons.math3.util.FastMath;
+import org.geotools.factory.Hints;
+import org.geotools.geometry.jts.JTS;
+import org.geotools.referencing.CRS;
+import org.opengis.geometry.MismatchedDimensionException;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.NoSuchAuthorityCodeException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.MathTransform;
+import org.opengis.referencing.operation.TransformException;
+
+import com.vividsolutions.jts.geom.Geometry;
 
 // Class for sorting functions that do standard vector operations
 public class Vector {
@@ -62,6 +73,55 @@ public class Vector {
             for (int i = 0; i < a.length; i++) {
             c[i] = a[i] + b[i];}
         return c;
-    }  
+    }
+    
+    /*
+     * Transform the coordinate reference system of the geometry
+     * 
+     * @param Geometry G. The geometry object to transform
+     * @param String epsgIn. The CRS of the input geometry in the format "EPSG:xxxx".
+     * @param String epsgOut. THe CRS to transform the input geometry to in the format "EPSG:xxxx"
+     * 
+     * @return Geometry. The transformed geometry object.
+     */
+    public static Geometry geomCRSTrans(Geometry G, String epsgIn, String epsgOut) {
 
+		CoordinateReferenceSystem sourceCRS = null;
+		try {
+			sourceCRS = CRS.decode(epsgIn);
+		} catch (FactoryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		CoordinateReferenceSystem targetCRS = null;
+		try {
+			targetCRS = CRS.decode(epsgOut);
+		} catch (FactoryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// Not sure what this line does and whether it is required
+		Hints.putSystemDefault(Hints.FORCE_LONGITUDE_FIRST_AXIS_ORDER, Boolean.TRUE);
+		
+		MathTransform transform = null;
+		try {
+			transform = CRS.findMathTransform(sourceCRS, targetCRS);
+		} catch (FactoryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Geometry targetGeometry = null;
+		try {
+			targetGeometry = JTS.transform( G, transform);
+		} catch (MismatchedDimensionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return targetGeometry;
+    }
 }
