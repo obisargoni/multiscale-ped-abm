ea9f50d09e38b292699c2afa80a0d1a666f57584
Obi Thompson Sargoni <33906525+obisargoni@users.noreply.github.com>
2019-04-29 13:40:53 +0100

    routing1 Separate, class specific projections for Junctions and RoadLinks. This allows me to use Nick Malleson's code for routing.

--- src/repastInterSim/environment/RoadLink.java
+++ src/repastInterSim/environment/RoadLink.java
@@ -9,7 +9,7 @@ public class RoadLink implements FixedGeography {
 	
 	private Geometry geom;
 	private List<Junction> junctions; // The Roads connected to this Junction, used in GIS road network
-	private NetworkEdge<Object> edge;
+	private NetworkEdge<Junction> edge;
 
 	
 	public RoadLink() {
@@ -31,11 +31,11 @@ public class RoadLink implements FixedGeography {
 		this.junctions.add(j);
 	}
 	
-	public void setEdge(NetworkEdge<Object> e) {
-		this.edge = e;
+	public void setEdge(NetworkEdge<Junction> edge2) {
+		this.edge = edge2;
 	}
 	
-	public NetworkEdge<Object> getEdge(){
+	public NetworkEdge<Junction> getEdge(){
 		return this.edge;
 	}
 	
