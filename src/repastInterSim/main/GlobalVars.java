6e70b3816c557f484b9cee5bbb796e594fb6ffc4
Obi Thompson Sargoni <33906525+obisargoni@users.noreply.github.com>
2019-05-01 16:55:31 +0100

    single_crs Switching to a single CRS (epsg 27700). Changing functions to get and move to a geometry so that they do not include or refer to a coord transform or multiple geometries.

--- src/repastInterSim/main/GlobalVars.java
+++ src/repastInterSim/main/GlobalVars.java
@@ -54,7 +54,7 @@ public abstract class GlobalVars {
 	public static String PedestrianRoadShapefile = "topographicAreaPedestrain_EPSG27700_Clipped_Single.shp";
 	public static String RoadLinkShapefile = "mastermap-itn RoadLink Clipped.shp";
 	public static String PedestrianObstructionShapefile = "topographicLineObstructing_EPSG27700_Clipped_RoadPathIntersection_Single.shp";
-	public static String StartingZonesFile = "StartZones.shp";
+	public static String StartingZonesFile = "StartZones_EPSG27700.shp";
 	public static String DestinationsFile = "destCoords.shp";
 	
 	public static final class GEOGRAPHY_PARAMS {
