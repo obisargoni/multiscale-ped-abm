b3df7ab45967822d5d9a7ed3351deeab22f467f9
Obi Thompson Sargoni <33906525+obisargoni@users.noreply.github.com>
2019-05-23 14:28:50 +0100

    vehicle1 Modifying the interface so that mobileAgents have an access method for their destination

--- src/repastInterSim/agent/mobileAgent.java
+++ src/repastInterSim/agent/mobileAgent.java
@@ -2,8 +2,11 @@ package repastInterSim.agent;
 
 import com.vividsolutions.jts.geom.Coordinate;
 
+import repastInterSim.environment.Destination;
+
 public interface mobileAgent {
 	
+	Destination getDestination();
 	void setLoc();
 	Coordinate getLoc();
 
