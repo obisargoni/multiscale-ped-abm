de01fd30ad085837f8b69afd8326fa03db121c6a
Obi Thompson Sargoni <33906525+obisargoni@users.noreply.github.com>
2019-05-24 10:52:08 +0100

    direction_correction Adding getter methods for accessing the Route of teh pedestrian and vehicle agents

--- src/repastInterSim/agent/mobileAgent.java
+++ src/repastInterSim/agent/mobileAgent.java
@@ -3,11 +3,13 @@ package repastInterSim.agent;
 import com.vividsolutions.jts.geom.Coordinate;
 
 import repastInterSim.environment.Destination;
+import repastInterSim.environment.Route;
 
 public interface mobileAgent {
 	
 	Destination getDestination();
 	void setLoc();
 	Coordinate getLoc();
+	Route getRoute();
 
 }
