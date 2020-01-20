9395b43eba946ab1059c23930f2c4bcd4a80c3e0
Obi Thompson Sargoni <33906525+obisargoni@users.noreply.github.com>
2020-01-20 17:47:34 +0000

    Data viz script plots grid values and flood fill values. Not very high quality but sufficient for now.

--- scripts/data_viz.py
+++ scripts/data_viz.py
@@ -5,19 +5,40 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
 import numpy as np
+import pandas as pd
 import csv
 
+# Input paths
 gridValuesFile = "..\\data\\output_grid_coverage_values.csv"
 floodFillValuesFile = "..\\data\\output_flood_fill_values.csv"
 
+# Output paths
+gridOutput = "..\\output\\gridValues.png"
+floodOutput = "..\\output\\floodValues.png"
+
+# Data input and cleaning
 gridValues = np.genfromtxt(gridValuesFile, delimiter=',')
+floodValues = np.genfromtxt(floodFillValuesFile, delimiter = ',')
 
-# Create 2d arrays to represent all possible dealer and player hands
-x = np.arange(gridValues[0].size) * np.ones(gridValues.size)[:, np.newaxis]
-y = np.ones([gridValues.size,gridValues[0].size]) * np.arange(gridValues.size)[:, np.newaxis]
+assert np.isnan(gridValues[:,-1]).all()
+assert np.isnan(floodValues[:,-1]).all()
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-#ax.plot_wireframe(x, y, z, rstride=1, cstride=1)
-surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,linewidth=0, antialiased=False)
-plt.show()
+gridValues = gridValues[:,:-1]
+floodValues = floodValues[:,:-1]
+
+max_int = 2147483647
+floodValues = np.where(floodValues == max_int, np.nan, floodValues)
+
+def plot_grid_image(array, colour_map, output_path = None):
+	fig, ax = plt.subplots()
+	im = ax.imshow(array, cmap=colour_map,origin='lower', extent=[0, array.shape[1], 0, array.shape[0]])
+	plt.colorbar(im)
+	if output_path is None:
+		plt.show()
+	else:
+		plt.savefig(output_path, bbox_inches='tight')
+
+	return fig, ax
+
+plot_grid_image(gridValues, cm.YlOrRd, gridOutput)
+plot_grid_image(floodValues, cm.YlOrRd, floodOutput)
