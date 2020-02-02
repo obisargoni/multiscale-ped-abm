9d8d06c9ae126023dfea280a93de931b08358e66
Obi Sargoni <othompsarg@hotmail.com>
2020-02-02 14:31:03 +0000

    Correcting typo in variable name

--- src/repastInterSim/main/GlobalVars.java
+++ src/repastInterSim/main/GlobalVars.java
@@ -152,7 +152,7 @@ public abstract class GlobalVars {
 	public static final class MOBILE_AGENT_PARAMS {
 		
 		public static double cautiousPriorityCostRatio = 10; // A pedestrian initialised with this perceives the cost of moving through vehicle priority space as 10x more costly than moving through ped priority space
-		public static double boldPriorityCostRation = 1; // Perceives cost of moving through vehicle space as the same as moving through pedestrian space
+		public static double boldPriorityCostRatio = 1; // Perceives cost of moving through vehicle space as the same as moving through pedestrian space
 		
 	}
 }
