3a67f2ec854b48b533ab225e0704929e2693728e
obisargoni <othompsarg@hotmail.com>
2020-02-07 12:55:03 +0000

    Removing unused import

--- src/repastInterSim/environment/RoadLink.java
+++ src/repastInterSim/environment/RoadLink.java
@@ -7,7 +7,6 @@ import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
-import com.vividsolutions.jts.geom.PrecisionModel;
 import com.vividsolutions.jts.geom.impl.CoordinateArraySequence;
 
 public class RoadLink implements FixedGeography {
