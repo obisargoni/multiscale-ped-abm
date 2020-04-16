174ed1236a24b7328ecb13deb01699e1a748b463
Obi Sargoni <othompsarg@hotmail.com>
2020-04-16 17:18:43 +0100

    Neatening up the script

--- scripts/gis_data_processing/selectTopographicAreaPedestrianVehicle.py
+++ scripts/gis_data_processing/selectTopographicAreaPedestrianVehicle.py
@@ -1,32 +1,55 @@
-# IPython log file
-
 import os
 import geopandas as gpd
 import pandas as pd
-#from operator import itemgetter
 
-clip_layer_file = "S:\\CASA_obits_ucfnoth\\1. PhD Work\\GIS Data\\CoventGardenWaterloo\\JunctClip.shp"
 
-data_directory = "S:\\CASA_obits_ucfnoth\\1. PhD Work\\GIS Data\\CoventGardenWaterloo\\mastermap-topo_2903032\\mastermap-topo_2903032_0 TopographicArea"
-fileTopo = 'mastermap TopographicArea.shp'
+################################
+#
+# Initalise data directories and file names
+#
+###############################
 
-output_directory = "S:\\CASA_obits_ucfnoth\\1. PhD Work\\GIS Data\\CoventGardenWaterloo\\Intersect Within layers"
-vehicle_file = "topographicAreaVehicle.shp"
-pedestrian_file = "topographicAreaPedestrian.shp"
+gis_data_dir = "S:\\CASA_obits_ucfnoth\\1. PhD Work\\GIS Data\\CoventGardenWaterloo"
+topographic_data_dir = os.path.join(gis_data_dir, "mastermap-topo_2903032\\mastermap-topo_2903032_0 TopographicArea")
+output_directory = os.path.join(gis_data_dir, "Intersect Within layers")
 
+if os.path.isdir(output_directory) == False:
+	os.mkdirs(output_directory)
+
+
+selection_layer_file = os.path.join(gis_data_dir, "JunctClip.shp")
+topographic_data_file = os.path.join(topographic_data_dir, 'mastermap TopographicArea.shp')
+output_vehicle_file = os.path.join(output_directory, "topographicAreaVehicle.shp")
+output_pedestrian_file = os.path.join(output_directory, "topographicAreaPedestrian.shp")
+
+projectCRS = {'init' :'epsg:27700'}
+
+priority_column = "priority"
+
+#################################
+#
 # Read in the data
-gdfTopoArea = gpd.read_file(os.path.join(data_directory,fileTopo))
-gdfClip = gpd.read_file(clip_layer_file)
+#
+#################################
+gdfTopoArea = gpd.read_file(topographic_data_file)
+gdfSelect = gpd.read_file(selection_layer_file)
 
 # Set the crs
-gdfTopoArea.crs = {'init' :'epsg:27700'}
-gdfClip.crs = {'init':'epsg:27700'}
+gdfTopoArea.crs = projectCRS
+gdfSelect.crs = projectCRS
 
 # Get the area to filter geometries by
-ClipArea = gdfClip.loc[0,'geometry']
+SelectPolygon = gdfSelect.loc[0,'geometry']
+
+
+################################
+#
+# Select just the pologyons of interest
+#
+################################
 
 # Select only the polygons that intersect or lie within the junc clip area
-gdfTopoArea = gdfTopoArea.loc[ (gdfTopoArea.geometry.intersects(ClipArea)) | (gdfTopoArea.geometry.within(ClipArea))]
+gdfTopoArea = gdfTopoArea.loc[ (gdfTopoArea.geometry.intersects(SelectPolygon)) | (gdfTopoArea.geometry.within(SelectPolygon))]
 
 
 # Could do some cleaning here - multipart to singlepart
@@ -42,6 +65,12 @@ gdfVehicle = gdfTopoArea.loc[gdfTopoArea['descriptiv'].isin([	'(1:Road Or Track)
 																'(2:Road Or Track,Structure)'])]
 
 
+
+###########################
+#
+# Save processed data
+#
+###########################
 # Save these polygon layers
-gdfPedestrian.to_file(os.path.join(output_directory, pedestrian_file))
-gdfVehicle.to_file(os.path.join(output_directory, vehicle_file))
+gdfPedestrian.to_file(os.path.join(output_directory, output_pedestrian_file))
+gdfVehicle.to_file(os.path.join(output_directory, output_vehicle_file))
