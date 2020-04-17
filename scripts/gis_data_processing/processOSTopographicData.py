9cfde5c65781c351dcb96f8f8b868104902d9d2b
Obi Sargoni <othompsarg@hotmail.com>
2020-04-17 16:38:16 +0100

    Including processing of ITN data in the script

--- scripts/gis_data_processing/processOSTopographicData.py
+++ scripts/gis_data_processing/processOSTopographicData.py
@@ -14,6 +14,11 @@ topographic_area_dir = os.path.join(gis_data_dir, "mastermap-topo_2903032\\maste
 topographic_line_dir = os.path.join(gis_data_dir, "mastermap-topo_2903032\\mastermap-topo_2903032_0 TopographicLine")
 
 
+itn_directory = os.path.join(gis_data_dir, "mastermap-itn_2903030")
+itn_link_file = os.path.join(itn_directory, "mastermap-itn RoadLink", "mastermap-itn 2903030_0 RoadLink.shp")
+itn_node_file = os.path.join(itn_directory, "mastermap-itn RoadNode", "mastermap-itn RoadNode.shp")
+
+
 output_directory = os.path.join(gis_data_dir, "processed_gis_data")
 
 if os.path.isdir(output_directory) == False:
@@ -29,21 +34,34 @@ output_vehicle_file = os.path.join(output_directory, "topographicAreaVehicle.shp
 output_pedestrian_file = os.path.join(output_directory, "topographicAreaPedestrian.shp")
 output_line_file = os.path.join(output_directory, "topographicLineObstructing_VehiclePedestrianIntersect.shp")
 
+output_itn_link_file = os.path.join(output_directory, "mastermap-itn RoadLink Intersect Within.shp")
+output_itn_node_file = os.path.join(output_directory, "mastermap-itn RoadNode Intersect Within.shp")
+
 
 projectCRS = {'init' :'epsg:27700'}
 
 priority_column = "priority"
+
 
 #################################
 #
 # Read in the data
 #
 #################################
-gdfTopoArea = gpd.read_file(topographic_area_file)
-gdfSelect = gpd.read_file(selection_layer_file)
 
-# Set the crs
+# Mastermap topographic area data  - for pavement and carriageway polygons
+gdfTopoArea = gpd.read_file(topographic_area_file)
 gdfTopoArea.crs = projectCRS
+
+# Mastermap ITN data - for road network
+gdfITNLink = gpd.read_file(itn_link_file)
+gdfITNLink.crs = projectCRS
+
+gdfITNNode = gpd.read_file(itn_node_file)
+gdfITNNode.crs = projectCRS
+
+# Study area polygon - to select data within the study area
+gdfSelect = gpd.read_file(selection_layer_file)
 gdfSelect.crs = projectCRS
 
 # Get the area to filter geometries by
@@ -52,13 +70,17 @@ SelectPolygon = gdfSelect.loc[0,'geometry']
 
 ################################
 #
-# Process topographic area data
+# Select the ITN Road Network that lies in the study area
 #
 ################################
 
 # Select only the polygons that intersect or lie within the junc clip area
-gdfTopoArea = gdfTopoArea.loc[ (gdfTopoArea.geometry.intersects(SelectPolygon)) | (gdfTopoArea.geometry.within(SelectPolygon))]
+gdfITNLink = gdfITNLink.loc[ (gdfITNLink.geometry.intersects(SelectPolygon)) | (gdfITNLink.geometry.within(SelectPolygon))]
+gdfITNNode = gpd.sjoin(gdfITNNode, gdfITNLink.loc[:,['fid','geometry']], op = 'intersects', lsuffix = 'node', rsuffix = 'line')
 
+# Clean up
+gdfITNNode.drop(['fid_line'], axis = 1, inplace=True)
+gdfITNNode.rename(columns = {'fid_node':'fid'}, inplace = True)
 
 # Could do some cleaning here - multipart to singlepart
 
@@ -112,4 +134,6 @@ gdfTopoLineSJ = gpd.sjoin(gdfTopoLine, gdfPedVeh, how = 'inner', op='intersects'
 # Save these polygon layers
 gdfPedestrian.to_file(output_pedestrian_file)
 gdfVehicle.to_file( output_vehicle_file)
-gdfTopoLineSJ.to_file(output_line_file)
+gdfTopoLineSJ.to_file(output_line_file)# Save the selected ITN geometries
+gdfITNLink.to_file(output_itn_link_file)
+gdfITNNode.to_file(output_itn_node_file)
