7b44dd39c8ff521eaccc54a189761972e78f609d
Obi Sargoni <othompsarg@hotmail.com>
2020-05-06 16:51:17 +0100

    Changing paths to inputs and outputs in editITN script. Now matches up with extract routing script and process OS data script

--- scripts/gis_data_processing/editITN.py
+++ scripts/gis_data_processing/editITN.py
@@ -13,23 +13,45 @@ import pandas as pd
 import geopandas as gpd
 import os
 
+
+######################
+#
+#
+# Initialise variables and paths to data inputs and outputs
+#
+#
+#####################
 crs = "epsg:27700"
 
-data_directory = "S:\\CASA_obits_ucfnoth\\1. PhD Work\\GIS Data\\CoventGardenWaterloo\\Intersect Within layers\\"
+gis_data_dir = "S:\\CASA_obits_ucfnoth\\1. PhD Work\\GIS Data\\CoventGardenWaterloo\\processed_gis_data"
+
 link_shapefile = "mastermap-itn RoadLink Intersect Within.shp"
 node_shapefile = "mastermap-itn RoadNode Intersect Within.shp"
 
-output_dir = "S:\\CASA_obits_ucfnoth\\1. PhD Work\\GIS Data\\CoventGardenWaterloo\\Intersect Within layers\\"
+route_info_dir = "S:\\CASA_obits_ucfnoth\\1. PhD Work\\GIS Data\\CoventGardenWaterloo\\itn_route_info"
+road_route_info_path = os.path.join(route_info_dir, "extracted_RRI.csv")
+road_node_info_path = os.path.join(route_info_dir, "extracted_RLNodes.csv")
+
+output_dir = gis_data_dir
 output_shapefile = "mastermap-itn RoadLink Intersect Within with orientation.shp"
 
+
+###########################
+#
+#
+# Load Data
+#
+#
+##########################
+
 # Read in data. Check geometry types. Check fields
-gdf_itn = gpd.read_file(os.path.join(data_directory, link_shapefile))
+gdf_itn = gpd.read_file(os.path.join(gis_data_dir, link_shapefile))
 
 assert gdf_itn['geometry'].type.unique().size == 1
 assert gdf_itn['fid'].unique().size == gdf_itn.shape[0]
 
 # Read in node information
-dfRLNode = pd.read_csv("extracted_RLNodes.csv")
+dfRLNode = pd.read_csv(road_node_info_path)
 
 # Join the itn road links with the node info to check that the -,+ nodes correspond to the first/last nodes of the linestring
 gdf_itn = pd.merge(gdf_itn, dfRLNode, left_on="fid", right_on="RoadLinkFID", how = "left", indicator=True)
@@ -38,7 +60,7 @@ assert gdf_itn.loc[ gdf_itn['_merge'] != 'both'].shape[0] == 0
 gdf_itn.drop('_merge', axis=1, inplace=True)
 
 # Load the node gis data
-gdf_itn_node = gpd.read_file(os.path.join(data_directory, node_shapefile))
+gdf_itn_node = gpd.read_file(os.path.join(gis_data_dir, node_shapefile))
 gdf_itn_node = gdf_itn_node.reindex(columns=['fid','geometry'])
 
 # Merge the nodes with the links
@@ -74,9 +96,9 @@ gdf_itn_node = None
 
 
 # With this check done, can now add the orientation to the itn road link data
-gdf_itn_link = gpd.read_file(os.path.join(data_directory, link_shapefile))
+gdf_itn_link = gpd.read_file(os.path.join(gis_data_dir, link_shapefile))
 
-dfRRI = pd.read_csv("extracted_RRI.csv")
+dfRRI = pd.read_csv(road_route_info_path)
 gdf_itn_link = pd.merge(gdf_itn_link, dfRRI, left_on='fid', right_on='DirectedLinkFID',how = 'left', indicator=True)
 gdf_itn_link = pd.merge(gdf_itn_link, dfRLNode, left_on='fid', right_on='RoadLinkFID',how = 'left', indicator=False)
 #assert gdf_itn_link.loc[ gdf_itn_link['_merge'] != 'both'].shape[0] ==0 # This fails, ok cos not all road links have orientation routing info
