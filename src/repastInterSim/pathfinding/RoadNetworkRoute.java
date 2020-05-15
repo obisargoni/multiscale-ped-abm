3e4c5025aefea34eab8f1c8fc2b3c08afe9f812a
Obi Sargoni <othompsarg@hotmail.com>
2020-05-15 11:57:33 +0100

    Change package name

--- src/repastInterSim/pathfinding/RoadNetworkRoute.java
+++ src/repastInterSim/pathfinding/RoadNetworkRoute.java
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with RepastCity.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-package pathfinding;
+package repastInterSim.pathfinding;
 
 import java.io.File;
 import java.io.FileInputStream;
