3468fd2bbf9dc7d7be36924a805708984b0b6b05
Obi Sargoni <othompsarg@hotmail.com>
2020-06-09 10:35:49 +0100

    Adding planning horizon attribute to pedestrian

--- src/repastInterSim/agent/Ped.java
+++ src/repastInterSim/agent/Ped.java
@@ -61,6 +61,8 @@ public class Ped extends MobileAgent {
     private HashMap<Integer, Double> gridSummandPriorityMap = new HashMap<Integer, Double>(); // Used to get grid cell summand value when running flood fill algorithm for routing
     private HashMap<Integer, Double> dynamicSummandPriorityMap = new HashMap<Integer, Double>();
     
+    private Integer pHorizon = 3;
+    
     private double vehiclePriorityCostRatio; // The ratio of pedestrian priority cell cost to vehicle priority cell cost. Represents pedestrian's perception of cost of moving in vehicle priority space.
     
     private int yieldTime = 0;
@@ -672,4 +674,14 @@ public class Ped extends MobileAgent {
     public List<GridCoordinates2D> getNextPathSection(){
     	return this.nextPathSection;
     }
+
+
+	public Integer getpHorizon() {
+		return pHorizon;
+	}
+
+
+	public void setpHorizon(Integer pHorizon) {
+		this.pHorizon = pHorizon;
+	}
 }
