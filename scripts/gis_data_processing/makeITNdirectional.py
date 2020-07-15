d8f28c774006037f5809c4f6dbca599b593d6205
Obi Sargoni <othompsarg@hotmail.com>
2020-07-15 19:31:46 +0100

    Moving creating of directed nx network from checkDiNetworkRouting to makeITNdirectional

--- scripts/gis_data_processing/makeITNdirectional.py
+++ scripts/gis_data_processing/makeITNdirectional.py
@@ -12,6 +12,8 @@ import numpy as np
 import pandas as pd
 import geopandas as gpd
 import os
+import json
+import networkx as nx
 
 
 ######################
@@ -21,6 +23,9 @@ import os
 #
 #
 #####################
+with open("config.json") as f:
+    config = json.load(f)
+
 crs = "epsg:27700"
 
 gis_data_dir = "S:\\CASA_obits_ucfnoth\\1. PhD Work\\GIS Data\\CoventGardenWaterloo\\processed_gis_data"
@@ -32,8 +37,12 @@ route_info_dir = "S:\\CASA_obits_ucfnoth\\1. PhD Work\\GIS Data\\CoventGardenWat
 road_route_info_path = os.path.join(route_info_dir, "extracted_RRI.csv")
 road_node_info_path = os.path.join(route_info_dir, "extracted_RLNodes.csv")
 
+output_vehicle_file = os.path.join(gis_data_dir, config["topo_vehicle_processed_file"])
+
+output_itn_edge_list = os.path.join(route_info_dir, "itn_edge_list.csv")
+
 output_dir = gis_data_dir
-output_shapefile = "mastermap-itn RoadLink Intersect Within with orientation.shp"
+output_shapefile = os.path.join(gis_data_dir, "mastermap-itn RoadLink Intersect Within with orientation.shp")
 
 
 ###########################
@@ -44,14 +53,34 @@ output_shapefile = "mastermap-itn RoadLink Intersect Within with orientation.shp
 #
 ##########################
 
+# Load ITN link data
+gdf_itn_link = gpd.read_file(os.path.join(gis_data_dir, link_shapefile))
+
 # Read in data. Check geometry types. Check fields
 gdf_itn = gpd.read_file(os.path.join(gis_data_dir, link_shapefile))
 
-assert gdf_itn['geometry'].type.unique().size == 1
-assert gdf_itn['fid'].unique().size == gdf_itn.shape[0]
+# Load road routing information
+dfRRI = pd.read_csv(road_route_info_path)
 
 # Read in node information
 dfRLNode = pd.read_csv(road_node_info_path)
+
+# Load ITN Nodes
+gdf_itn_node = gpd.read_file(os.path.join(gis_data_dir, node_shapefile))
+
+gdfVehicleLinks = gpd.read_file(output_vehicle_file)
+
+
+##########################
+#
+#
+# Check ITN data
+#
+#
+##########################
+
+assert gdf_itn['geometry'].type.unique().size == 1
+assert gdf_itn['fid'].unique().size == gdf_itn.shape[0]
 
 # Join the itn road links with the node info to check that the -,+ nodes correspond to the first/last nodes of the linestring
 gdf_itn = pd.merge(gdf_itn, dfRLNode, left_on="fid", right_on="RoadLinkFID", how = "left", indicator=True)
@@ -59,8 +88,7 @@ gdf_itn = pd.merge(gdf_itn, dfRLNode, left_on="fid", right_on="RoadLinkFID", how
 assert gdf_itn.loc[ gdf_itn['_merge'] != 'both'].shape[0] == 0
 gdf_itn.drop('_merge', axis=1, inplace=True)
 
-# Load the node gis data
-gdf_itn_node = gpd.read_file(os.path.join(gis_data_dir, node_shapefile))
+# Select just the node fields needed
 gdf_itn_node = gdf_itn_node.reindex(columns=['fid','geometry'])
 
 # Merge the nodes with the links
@@ -89,16 +117,17 @@ assert gdf_itn['first_coords_match_minus_node'].all() == True
 gdf_itn['last_coords_match_plus_node'].all() == True
 
 gdf_itn = None
-gdf_itn_node = None
 
 
-#######################################################################################
 
+###########################
+#
+#
+# With checks done, can now add in routing info
+#
+#
+###########################
 
-# With this check done, can now add the orientation to the itn road link data
-gdf_itn_link = gpd.read_file(os.path.join(gis_data_dir, link_shapefile))
-
-dfRRI = pd.read_csv(road_route_info_path)
 gdf_itn_link = pd.merge(gdf_itn_link, dfRRI, left_on='fid', right_on='DirectedLinkFID',how = 'left', indicator=True)
 gdf_itn_link = pd.merge(gdf_itn_link, dfRLNode, left_on='fid', right_on='RoadLinkFID',how = 'left', indicator=False)
 #assert gdf_itn_link.loc[ gdf_itn_link['_merge'] != 'both'].shape[0] ==0 # This fails, ok cos not all road links have orientation routing info
@@ -123,4 +152,60 @@ print(gdf_itn_link['direction'].value_counts())
 print(gdf_itn_link['DirectedLinkOrientation'].value_counts())
 
 
-gdf_itn_link.to_file(os.path.join(output_dir, output_shapefile))
+##################################
+#
+#
+# Create Directed Road Network from ITN Data adn Routing Info
+#
+#
+##################################
+# Filter the nodes to be just those in the ITN Link data used for the model
+dfRLNode = dfRLNode.loc[ dfRLNode["RoadLinkFID"].isin(gdf_itn_link.fid)]
+
+# Check all road links are present and that thy all have 2 nodes
+assert len(dfRLNode.RoadLinkFID.unique()) == len(gdf_itn_link.fid.unique())
+assert dfRLNode['PlusNodeFID'].isnull().any() == False
+assert dfRLNode['MinusNodeFID'].isnull().any() == False
+
+# Merge with data on which links are directed
+dfRLNode = pd.merge(dfRLNode, dfRRI, left_on = "RoadLinkFID", right_on = "DirectedLinkFID", how = "left", indicator=True)
+dfRLNode.drop("_merge", axis = 1, inplace = True)
+
+# Merge with link weight and id data
+dfRLNode = pd.merge(dfRLNode, gdf_itn_link.reindex(columns = ['fid','weight']), left_on = "RoadLinkFID", right_on = "fid", how = "outer", indicator = True)
+
+# Now build the edge list
+dfEdgeList = pd.DataFrame(columns = ['start_node', 'end_node'])
+dfRLNode.drop("_merge", axis = 1, inplace = True)
+
+# df1 are teh links that go from minus node to plus node, orientation = +
+df1 = dfRLNode.loc[dfRLNode['DirectedLinkOrientation'] == "+"].reindex(columns = ['MinusNodeFID','PlusNodeFID', "RoadLinkFID", "weight"])
+# df2 are the nodes that go from plus to minus
+df2 = dfRLNode.loc[dfRLNode['DirectedLinkOrientation'] == "-"].reindex(columns = ['PlusNodeFID','MinusNodeFID', "RoadLinkFID", "weight"])
+# df3 are the non directed link, part 1
+df3 = dfRLNode.loc[dfRLNode['DirectedLinkOrientation'].isnull()].reindex(columns = ['MinusNodeFID','PlusNodeFID', "RoadLinkFID", "weight"])
+# df4 are the non directed links part 2
+df4 = dfRLNode.loc[dfRLNode['DirectedLinkOrientation'].isnull()].reindex(columns = ['PlusNodeFID','MinusNodeFID', "RoadLinkFID", "weight"])
+
+df1.rename(columns = {'MinusNodeFID':'start_node', 'PlusNodeFID':'end_node'}, inplace=True)
+df2.rename(columns = {'PlusNodeFID':'start_node', 'MinusNodeFID':'end_node'}, inplace=True)
+df3.rename(columns = {'PlusNodeFID':'start_node', 'MinusNodeFID':'end_node'}, inplace=True)
+df4.rename(columns = {'MinusNodeFID':'start_node', 'PlusNodeFID':'end_node'}, inplace=True)
+
+dfEdgeList = pd.concat([df1,df2,df3,df4], join = "outer")
+dfEdgeList = dfEdgeList.reindex(columns=['start_node','end_node', 'RoadLinkFID','weight'])
+
+
+# Now create the network
+graphRL = nx.from_pandas_edgelist(dfEdgeList, source='start_node', target = 'end_node', edge_attr = ['RoadLinkFID','weight'], create_using=nx.DiGraph())
+assert graphRL.is_directed()
+
+#############################
+#
+#
+# Save data
+#
+#
+#############################
+gdf_itn_link.to_file(output_shapefile)
+dfEdgeList.to_csv(output_itn_edge_list)
