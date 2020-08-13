f1f4b279e0df5ec138d580578fe295f1677c6b4b
Obi Sargoni <othompsarg@hotmail.com>
2020-08-13 16:40:51 +0100

    Method to calculate the sampling probabilities for each crossing alternative

--- src/repastInterSim/pathfinding/AccumulatorRoute.java
+++ src/repastInterSim/pathfinding/AccumulatorRoute.java
@@ -24,5 +24,30 @@ public class AccumulatorRoute {
 		this.cas = new ArrayList<CrossingAlternative>();
 		
 	}
+	/*
+	 * Calculate the probability of sampling each crossing alternative using the softmax function
+	 * with the argument given by the distance to the crossing alternative from the pedestrian multiplied by the
+	 * pedestrian's lambda factor.
+	 */
+	private List<Double> caSamplingProbabilities(){
+		List<Double> probs = new ArrayList<Double>();
+		
+		double lambda = this.ped.getLambda();
+		
+		// Loop through crossing alternatives and find distance from ped to each one
+		List<Double> distances = new ArrayList<Double>();
+		for (CrossingAlternative ca: this.cas) {
+			Double d = ca.distanceTo(ped.getLoc());
+			
+			// Multiply distance by ped attribute lambda to control effect of distance on probability
+			distances.add(d*lambda);
+		}
+		
+		// Calculate sampling probability using softmax function
+	    double total = distances.stream().map(Math::exp).mapToDouble(f->f).sum();
+	    probs = distances.stream().map(f-> (Math.exp(f) / total)).collect(Collectors.toList());
+		
+		return probs;
+	}
 
 }
