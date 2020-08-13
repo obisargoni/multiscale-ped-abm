a59885058bfe79f8fef94131efc8e03202d09565
Obi Sargoni <othompsarg@hotmail.com>
2020-08-13 15:41:34 +0100

    Adding roadLinkID attribute and method for getting number of vehicle on road link crossing is on

--- src/repastInterSim/environment/CrossingAlternative.java
+++ src/repastInterSim/environment/CrossingAlternative.java
@@ -2,6 +2,8 @@ package repastInterSim.environment;
 
 import com.vividsolutions.jts.geom.Coordinate;
 
+import repast.simphony.space.gis.Geography;
+
 public class CrossingAlternative {
 	
 	// Coordinates at which the ca meets pavement
@@ -10,10 +12,35 @@ public class CrossingAlternative {
 	
 	// Default type is unmarked
 	private String type = "unmarked";
+	
+	// id of the road link this crossing is located on
+	private String roadLinkID;
 
 	public CrossingAlternative(){
+				
 	}
 	
+	public String getRoadLinkID() {
+		return roadLinkID;
+	}
+
+	public void setRoadLinkID(String roadLinkID) {
+		this.roadLinkID = roadLinkID;
+	}
+
+	public Integer getvFlow(Geography<Road> rG) {
+		// Get the number of vehicles on the road link
+		Road r = null;
+    	for (Road ri: rG.getAllObjects()) {
+    		if (ri.getRoadLinkID().contentEquals(this.roadLinkID)){
+    			r = ri;
+    		}
+    	}
+    	int vehicleNumber = r.getRoadLinksVehicleCount();
+		return vehicleNumber;
+	}
+
+
 	public Coordinate getC1() {
 		return c1;
 	}
@@ -37,5 +64,6 @@ public class CrossingAlternative {
 	public void setType(String type) {
 		this.type = type;
 	}
+	
 
 }
