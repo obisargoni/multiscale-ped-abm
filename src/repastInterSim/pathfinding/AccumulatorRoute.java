2b9c54b816fbbfc21bc98df0d8f05133ca5baea6
Obi Sargoni <othompsarg@hotmail.com>
2020-08-14 11:15:40 +0100

    Initialising crossing alternative activations array

--- src/repastInterSim/pathfinding/AccumulatorRoute.java
+++ src/repastInterSim/pathfinding/AccumulatorRoute.java
@@ -7,22 +7,28 @@ package repastInterSim.pathfinding;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
+
+import com.vividsolutions.jts.geom.Coordinate;
 
 import repastInterSim.agent.Ped;
 import repastInterSim.environment.CrossingAlternative;
-import repastInterSim.environment.OD;
 
 public class AccumulatorRoute {
 	
 	private Ped ped;
 	
 	private List<CrossingAlternative> cas;
+	private double[] caActivations;
 	
 	public AccumulatorRoute(Ped p) {
 		this.ped = p;
 		this.cas = new ArrayList<CrossingAlternative>();
-		
+		this.caActivations = new double[this.cas.size()];
+		for (int i=0;i<this.caActivations.length; i++) {
+			this.caActivations[i]=0;
+		}
 	}
 	/*
 	 * Calculate the probability of sampling each crossing alternative using the softmax function
