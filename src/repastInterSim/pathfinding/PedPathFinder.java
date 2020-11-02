2a50b5e025101c94e277e00f830046eb326396a6
Obi Sargoni <othompsarg@hotmail.com>
2020-11-02 17:32:31 +0000

    Pass coordinates to RoadNetworkRoute initialisation rather than OD geography.

--- src/repastInterSim/pathfinding/PedPathFinder.java
+++ src/repastInterSim/pathfinding/PedPathFinder.java
@@ -73,9 +73,9 @@ public class PedPathFinder {
 	 * Use this to identify the shortest path through the network and assign this path to this classes' strategic path attribute.
 	 * 
 	 */
-	public void planStrategicPath(OD o, OD d, Geography<RoadLink> rlG, Network<Junction> pedNet, Geography<OD> odG) {
+	public void planStrategicPath(Coordinate oC, Coordinate dC, Geography<RoadLink> rlG, Network<Junction> pedNet, Geography<OD> odG) {
 		// Initialise road network route - needs to ne non-directed for pedestrians! fix this
-		RoadNetworkRoute rnr = new RoadNetworkRoute(o.getGeom().getCoordinate(), d.getGeom().getCoordinate(), rlG, pedNet, odG);
+		RoadNetworkRoute rnr = new RoadNetworkRoute(oC, dC, rlG, pedNet, odG);
 		
 		// Find shortest path using road network route
 		try {
