f09ed1c588fd62c107dadede39d7421fe1c381b9
Obi Sargoni <othompsarg@hotmail.com>
2020-11-09 17:34:29 +0000

    Separating UnmarkedCrossingAlternative from DedicatedCrossingAlternative

--- src/repastInterSim/environment/DedicatedCrossingAlternative.java
+++ src/repastInterSim/environment/DedicatedCrossingAlternative.java
@@ -14,7 +14,7 @@ import repastInterSim.exceptions.RoutingException;
 import repastInterSim.main.GlobalVars;
 import repastInterSim.pathfinding.RoadNetworkRoute;
 
-public class DedicatedCrossingAlternative implements CrossingAlternative {
+public class DedicatedCrossingAlternative implements CrossingAlternative, FixedGeography {
 	
 	// Coordinates at which the ca meets pavement
 	private Geometry geom = null;
@@ -24,19 +24,11 @@ public class DedicatedCrossingAlternative implements CrossingAlternative {
 	// Destination coordinate. This is the destination this crossing alternative leads to
 	private Coordinate destination = null;
 	
-	// Default type is unmarked
-	private String type = "unmarked";
+	private String type;
 	
 	// id of the road link this crossing is located on
 	private String roadLinkID;
 	private Road road;
-	
-	// pedestrian agent perceiving this crossing
-	private Ped ped = null;
-	
-	// Road geography containing the pavement and carriageway polygons
-	private Geography<Road> roadGeography = null;
-	private List<RoadLink> strategicPathsection;
 
 	public DedicatedCrossingAlternative(){
 				
@@ -83,73 +75,19 @@ public class DedicatedCrossingAlternative implements CrossingAlternative {
 	}
 	
 	public String getRoadLinkID() {
-		if (this.roadLinkID == null) {
-			return this.getRoad().getRoadLinkID();
-		}
-		else {
-			return this.roadLinkID;
-		}
+		return this.roadLinkID;
 	}
 	
 	public Road getRoad() {
-		if (this.road == null) {
-			return this.ped.getCurrentRoad();
-		}
-		else {
-			return this.road;
-		}
+		return this.road;
 	}
 
-	public void setRoad() {
-		this.road = this.ped.getCurrentRoad();
-	}
-
-	/*
-	 * Get the nearest coordinate on the pavement on the opposite side of the road to the input coordinate.
-	 * Used to identify the end point of unmarked crossing alternatives.
-	 */
-	public Coordinate nearestOppositePedestrianCoord(Coordinate c, String roadLinkID, Geography<Road> rG, List<RoadLink> sps) {
-		
-		// Get pedestrian road objects on this road link
-		List<Road> caPedRoads = null;
-		try {
-			caPedRoads = RoadNetworkRoute.getRoadLinkPedestrianRoads(roadLinkID, rG);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		// Loop through ped roads and find nearest coordinate on each
-		Double minDist = Double.MAX_VALUE;
-		Coordinate nearestOpCoord = null;
-		for (Road rd:caPedRoads) {
-			// Get nearest coord
-			Coordinate nearC = GISFunctions.xestGeomCoordinate(c, rd.getGeom(), false);
-			
-			// Check parity to make sure coordinate is on opposite side of the road
-			int p = RoadNetworkRoute.calculateRouteParity(c, nearC, sps);
-			if (p==0) {
-				continue;
-			}
-			
-			// Check if nearer
-			double d = c.distance(nearC);
-			if (d < minDist) {
-				minDist = d;
-				nearestOpCoord = nearC;
-			}
-		}
-		
-		return nearestOpCoord;
+	public void setRoad(Road r) {
+		this.road = r;
 	}
 
 	public Coordinate getC1() {
-		if (c1==null) {
-			return this.ped.getLoc();
-		}
-		else {
-			return c1;
-		}
+		return c1;
 	}
 
 	public void setC1(Coordinate c1) {		
@@ -157,12 +95,7 @@ public class DedicatedCrossingAlternative implements CrossingAlternative {
 	}
 
 	public Coordinate getC2() {
-		if(c2==null) {
-			return nearestOppositePedestrianCoord(this.ped.getLoc(), this.getRoadLinkID(), this.roadGeography, this.strategicPathsection);
-		}
-		else {
-			return c2;
-		}
+		return c2;
 	}
 
 	public void setC2(Coordinate c2) {
@@ -185,22 +118,6 @@ public class DedicatedCrossingAlternative implements CrossingAlternative {
 		this.destination = destination;
 	}
 
-	public Ped getPed() {
-		return ped;
-	}
-
-	public void setPed(Ped ped) {
-		this.ped = ped;
-	}
-
-	public Geography<Road> getRoadGeography() {
-		return roadGeography;
-	}
-
-	public void setRoadGeography(Geography<Road> roadGeography) {
-		this.roadGeography = roadGeography;
-	}
-
 	@Override
 	public Geometry getGeom() {
 		return this.geom;
@@ -214,9 +131,4 @@ public class DedicatedCrossingAlternative implements CrossingAlternative {
 		this.c1 = g.getCoordinates()[0];
 		this.c2 = g.getCoordinates()[ncoords-1];
 	}
-
-	public void setStrategicPathSection(List<RoadLink> sps) {
-		this.strategicPathsection = sps;		
-	}
-
 }
--- src/repastInterSim/environment/UnmarkedCrossingAlternative.java
+++ src/repastInterSim/environment/UnmarkedCrossingAlternative.java
@@ -1,5 +1,7 @@
 package repastInterSim.environment;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 import com.vividsolutions.jts.geom.Coordinate;
@@ -7,11 +9,15 @@ import com.vividsolutions.jts.geom.Geometry;
 
 import repast.simphony.space.gis.Geography;
 import repastInterSim.agent.Ped;
+import repastInterSim.pathfinding.RoadNetworkRoute;
 
 public class UnmarkedCrossingAlternative implements CrossingAlternative {
 	
 	private Ped ped;
 	
+	private String type = "unmarked";
+	private Coordinate destination;
+		
 	// Road geography containing the pavement and carriageway polygons
 	private Geography<Road> roadGeography = null;
 	private List<RoadLink> strategicPathsection;
@@ -20,35 +26,34 @@ public class UnmarkedCrossingAlternative implements CrossingAlternative {
 	public UnmarkedCrossingAlternative() {
 		// TODO Auto-generated constructor stub
 	}
-
-	@Override
-	public Geometry getGeom() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setGeom(Geometry c) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public Coordinate getC1() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Coordinate getC2() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
+	
+	/*
+	 * Calculate the distance to the input coordinate. Distance return is the distance to the nearest
+	 * crossing alternative coordiante
+	 * 
+	 * @param Coordinate loc
+	 * 		The coordinate to calculate the distance from
+	 */
 	public Double distanceTo(Coordinate loc) {
-		// TODO Auto-generated method stub
-		return null;
+		double d1 = getC1().distance(loc);
+		double d2 = getC2().distance(loc);
+		return Math.min(d1, d2);
+	}
+
+	public Coordinate nearestCoord(Coordinate loc) {
+		Coordinate[] coords = {getC1(), getC2()};
+		
+		Coordinate cNear = Arrays.stream(coords).min(Comparator.comparing(c->c.distance(loc))).get();
+		
+		return cNear;
+	}
+
+	public Coordinate farthestCoord(Coordinate loc) {
+		Coordinate[] coords = {getC1(), getC2()};
+		
+		Coordinate cFar = Arrays.stream(coords).max(Comparator.comparing(c->c.distance(loc))).get();
+		
+		return cFar;
 	}
 
 	@Override
@@ -60,57 +65,80 @@ public class UnmarkedCrossingAlternative implements CrossingAlternative {
 		int vehicleNumber = this.getRoad().getRoadLinksVehicleCount();
 		return vehicleNumber;
 	}
-
-	@Override
-	public Road getRoad() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setRoad() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public String getType() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setType(String type) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public Coordinate getDestination() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setDestination(Coordinate d) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public Coordinate nearestCoord(Coordinate loc) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Coordinate farthestCoord(Coordinate loc) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public String getRoadLinkID() {
+		return this.getRoad().getRoadLinkID();
 	}
 	
-	public void setPed(Ped p) {
-		this.ped = p;
+	public Road getRoad() {
+		return this.ped.getCurrentRoad();
+	}
+	
+	/*
+	 * Get the nearest coordinate on the pavement on the opposite side of the road to the input coordinate.
+	 * Used to identify the end point of unmarked crossing alternatives.
+	 */
+	public Coordinate nearestOppositePedestrianCoord(Coordinate c, String roadLinkID, Geography<Road> rG, List<RoadLink> sps) {
+		
+		// Get pedestrian road objects on this road link
+		List<Road> caPedRoads = null;
+		try {
+			caPedRoads = RoadNetworkRoute.getRoadLinkPedestrianRoads(roadLinkID, rG);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// Loop through ped roads and find nearest coordinate on each
+		Double minDist = Double.MAX_VALUE;
+		Coordinate nearestOpCoord = null;
+		for (Road rd:caPedRoads) {
+			// Get nearest coord
+			Coordinate nearC = GISFunctions.xestGeomCoordinate(c, rd.getGeom(), false);
+			
+			// Check parity to make sure coordinate is on opposite side of the road
+			int p = RoadNetworkRoute.calculateRouteParity(c, nearC, sps);
+			if (p==0) {
+				continue;
+			}
+			
+			// Check if nearer
+			double d = c.distance(nearC);
+			if (d < minDist) {
+				minDist = d;
+				nearestOpCoord = nearC;
+			}
+		}
+		
+		return nearestOpCoord;
+	}
+	
+	public Coordinate getC1() {
+		return this.ped.getLoc();
+	}
+
+	public Coordinate getC2() {
+		return nearestOppositePedestrianCoord(this.ped.getLoc(), this.getRoadLinkID(), this.roadGeography, this.strategicPathsection);
+	}
+	
+	public String getType() {
+		return type;
+	}
+
+	public Coordinate getDestination() {
+		return destination;
+	}
+
+	public void setDestination(Coordinate destination) {
+		this.destination = destination;
+	}
+
+	public Ped getPed() {
+		return ped;
+	}
+
+	public void setPed(Ped ped) {
+		this.ped = ped;
 	}
 	
 	public Geography<Road> getRoadGeography() {
