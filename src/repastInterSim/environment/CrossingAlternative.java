91fa8f90008fa8fba2e842945df3a775f1d139f1
Obi Sargoni <othompsarg@hotmail.com>
2020-11-09 17:34:11 +0000

    Adjusting the interface to include all the required methods and not inherit FixedGeography

--- src/repastInterSim/environment/CrossingAlternative.java
+++ src/repastInterSim/environment/CrossingAlternative.java
@@ -2,19 +2,19 @@ package repastInterSim.environment;
 
 import com.vividsolutions.jts.geom.Coordinate;
 
-public interface CrossingAlternative extends FixedGeography {
+public interface CrossingAlternative {
 	
-	public Coordinate getC1();
-	public Coordinate getC2();
 	public Double distanceTo(Coordinate loc);
-	public Integer getvFlow();
-	public Road getRoad();
-	public void setRoad();
-	public String getType();
-	public void setType(String type);
-	public Coordinate getDestination();
-	public void setDestination(Coordinate d);
 	public Coordinate nearestCoord(Coordinate loc);
 	public Coordinate farthestCoord(Coordinate loc);
+	public Integer getvFlow();
+	public String getRoadLinkID();
+	public Road getRoad();
+	public Coordinate getC1();
+	public Coordinate getC2();
+	public String getType();
+	public Coordinate getDestination();
+	public void setDestination(Coordinate d);
+
 
 }
