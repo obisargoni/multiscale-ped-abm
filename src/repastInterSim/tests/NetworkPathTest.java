20318fd4053e83abaff417691074e9b57a52a641
Obi Sargoni <othompsarg@hotmail.com>
2021-02-18 17:56:17 +0000

    Adding test of the NetworkPath class. Testing method to get simple paths with and without filters

--- src/repastInterSim/tests/NetworkPathTest.java
+++ src/repastInterSim/tests/NetworkPathTest.java
@@ -3,11 +3,15 @@ package repastInterSim.tests;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 import org.apache.commons.collections15.Predicate;
 
 import org.junit.jupiter.api.Test;
+
+import com.vividsolutions.jts.geom.Coordinate;
 
 import repast.simphony.context.Context;
 import repast.simphony.context.space.gis.GeographyFactoryFinder;
@@ -27,8 +31,12 @@ import repastInterSim.environment.contexts.RoadLinkContext;
 import repastInterSim.main.GlobalVars;
 import repastInterSim.main.IO;
 import repastInterSim.pathfinding.NetworkPath;
+import repastInterSim.pathfinding.RoadNetworkRoute;
 
 class NetworkPathTest {
+	
+	Geography<RoadLink> roadLinkGeography = null;
+	Network<Junction> roadNetwork = null;
 	
 	Context<Junction> pavementJunctionContext = null;
 	Geography<RoadLink> pavementLinkGeography = null;
@@ -59,8 +67,25 @@ class NetworkPathTest {
 		return rlG;
 	}
 	
+	void setUpRoadLinks(String roadLinkFile) throws Exception {
+		roadLinkGeography = setUpLinks(roadLinkFile);
+	}
+	
 	void setUpPavementLinks(String linkFile) throws MalformedURLException, FileNotFoundException {
 		pavementLinkGeography = setUpLinks(linkFile);
+	}
+	
+	void setUpRoadNetwork(boolean isDirected) {
+		Context<Junction> junctionContext = new JunctionContext();
+		GeographyParameters<Junction> GeoParamsJunc = new GeographyParameters<Junction>();
+		Geography<Junction> junctionGeography = GeographyFactoryFinder.createGeographyFactory(null).createGeography("junctionGeography", junctionContext, GeoParamsJunc);
+		junctionGeography.setCRS(GlobalVars.geographyCRSString);
+		
+		NetworkBuilder<Junction> builder = new NetworkBuilder<Junction>(GlobalVars.CONTEXT_NAMES.ROAD_NETWORK,junctionContext, isDirected);
+		builder.setEdgeCreator(new NetworkEdgeCreator<Junction>());
+		roadNetwork = builder.buildNetwork();
+		
+		GISFunctions.buildGISRoadNetwork(roadLinkGeography, junctionContext, junctionGeography, roadNetwork);
 	}
 	
 	void setUpPavementNetwork() {
@@ -84,6 +109,50 @@ class NetworkPathTest {
 		GISFunctions.readShapefile(Junction.class, pedJPath, pavementJunctionGeography, pavementJunctionContext);
 		SpatialIndexManager.createIndex(pavementJunctionGeography, Junction.class);
 	}
+	
+	List<RoadLink> planStrategicPath(Coordinate o, Coordinate d, String j1ID, String j2ID){
+		
+		// Plan the strategic path
+		List<RoadLink> sP = new ArrayList<RoadLink>();		
+		RoadNetworkRoute rnr = new RoadNetworkRoute(o , d);
+		
+		// Define my starting and ending junctions to test
+		// Need to to this because although the origin and destination were selected above, this was just to initialise RoadNetworkRoute with
+		// The route is actually calculated using junctions. 
+		List<Junction> currentJunctions = new ArrayList<Junction>();
+		List<Junction> destJunctions = new ArrayList<Junction>();
+		for(Junction j: roadNetwork.getNodes()) {
+			
+			// Set the test current junctions 
+			if (j.getFID().contentEquals(j1ID)) {
+				currentJunctions.add(j);
+			}
+			
+			// Set the test destination junctions
+			if (j.getFID().contentEquals(j2ID)) {
+				destJunctions.add(j);
+			}
+		}
+		
+		Junction[] routeEndpoints = new Junction[2];
+		
+		// Get shortest Route according to the Route class
+		List<RepastEdge<Junction>> shortestRoute = null;
+		try {
+			shortestRoute = rnr.getShortestRoute(roadNetwork, currentJunctions, destJunctions, routeEndpoints, false);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		for(int i=0; i< shortestRoute.size(); i++) {
+			NetworkEdge<Junction> e = (NetworkEdge<Junction>)shortestRoute.get(i);
+			sP.add(e.getRoadLink());
+		}
+		
+		return sP;
+	}
+
 	
 	@Test
 	void testPathsBetweenNodePavementJunctions1() {
@@ -171,6 +240,107 @@ class NetworkPathTest {
 		// Check that the edge path for this single entry node path is empty
 		Stack<RepastEdge<Junction>> edgePath = selfPaths.get(0);
 		assert edgePath.size()==0;
+	}
+	
+	@Test
+	void testFilterNetwork() {
+		try {
+			setUpPavementJunctions();
+			setUpPavementLinks("pedNetworkLinks.shp");
+			setUpPavementNetwork();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		NetworkPath<Junction> np = new NetworkPath<Junction>(this.pavementNetwork);
+		
+		// Check initial number of nodes
+		int nNodes = 0;
+		for (Junction n : this.pavementNetwork.getNodes()) {
+			nNodes++;
+		}
+		assert nNodes == 125;
+		
+		assert np.getGraph().getVertexCount() == 125;
+		
+		// Now filter to include all road nodes in the strategic path and check number of simple paths returned
+		String[] tacticalPavementNodesArray = {"pave_node_79","pave_node_87","pave_node_88","pave_node_80","pave_node_81","pave_node_82","pave_node_83","pave_node_84","pave_node_85","pave_node_86","pave_node_89","pave_node_90","pave_node_91","pave_node_111","pave_node_112","pave_node_113","pave_node_114"};
+		List<String> tacticalPavementNodeIDs = Arrays.asList(tacticalPavementNodesArray);
+		
+		//filter = j -> sPNodes.stream().anyMatch( n -> n.getFID().contentEquals(j.getjuncNodeID()));
+		Predicate<Junction> filter = j -> tacticalPavementNodeIDs.stream().anyMatch(n -> n.contentEquals(j.getFID()));
+		np.filterGraph(filter);
+		assert np.getGraph().getVertexCount() == tacticalPavementNodeIDs.size();
+		
+		
+		// Filter again this time using a collection
+		List<Junction> tacticalPavementNodes = new ArrayList<Junction>();
+		for (Junction j: this.pavementJunctionGeography.getAllObjects()) {
+			if (tacticalPavementNodeIDs.stream().anyMatch(n -> n.contentEquals(j.getFID()))) {
+				tacticalPavementNodes.add(j);
+			}
+		}
+		
+		assert tacticalPavementNodes.size() == tacticalPavementNodeIDs.size();
+		
+		np = new NetworkPath<Junction>(this.pavementNetwork);
+		np.filterGraph(tacticalPavementNodes);
+		assert np.getGraph().getVertexCount() == tacticalPavementNodes.size();
+	}
+	
+	@Test
+	void testSimplePathsAlongTacticalHorizon() {
+		
+		// setup road network and pavement network
+		try {
+			setUpRoadLinks("open-roads RoadLink Intersect Within simplify angles.shp");
+			setUpRoadNetwork(false);
+			
+			setUpPavementJunctions();
+			setUpPavementLinks("pedNetworkLinks.shp");
+			setUpPavementNetwork();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String sourceID = "pave_node_85";
+		String targetID = "pave_node_112";
+		Junction source = null;
+		Junction target = null;
+		for (Junction j : this.pavementJunctionGeography.getAllObjects()) {
+			if (j.getFID().contentEquals(sourceID)) {
+				source = j;
+			}
+			else if (j.getFID().contentEquals(targetID)) {
+				target = j;
+			}
+		}
+		
+		String startJunctionID = source.getjuncNodeID();
+		String endJunctionID = target.getjuncNodeID();
+		Coordinate o = null;
+		Coordinate d = null;
+		List<RoadLink> sP = planStrategicPath(o, d, startJunctionID, endJunctionID);
+		
+		List<Junction> sPNodes = new ArrayList<Junction>();
+		for (RoadLink rl: sP) {
+			rl.getJunctions().stream().forEach(sPNodes::add);
+		}
+		
+		NetworkPath<Junction> np = new NetworkPath<Junction>(this.pavementNetwork);
+
+		// First test that when filter excludes target node, zero simple paths returned
+		String firstJunctionID = sPNodes.get(0).getFID();
+		Predicate<Junction> filter = j -> j.getjuncNodeID().contentEquals(firstJunctionID);
+		List<Stack<RepastEdge<Junction>>> paths = np.getSimplePaths(source, target, filter);
+		assert paths.size() == 0;
+		
+		// Now change filter to include all road nodes in the strategic path and check number of simple paths returned
+		filter = j -> sPNodes.stream().anyMatch( n -> n.getFID().contentEquals(j.getjuncNodeID()));
+		paths = np.getSimplePaths(source, target, filter);
+		assert paths.size() == 2076; // python +networkx gives me 2076
 	}
 
 }
