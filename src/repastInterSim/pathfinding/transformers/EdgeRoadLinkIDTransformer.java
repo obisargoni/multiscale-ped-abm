4e25ab1772a962d33cc5e7826a53fc212d69f63f
Obi Sargoni <othompsarg@hotmail.com>
2021-02-22 15:43:33 +0000

    New transformer that determines whether link crosses road based on the ped road link ID

---
+++ src/repastInterSim/pathfinding/transformers/EdgeRoadLinkIDTransformer.java
@@ -0,0 +1,40 @@
+package repastInterSim.pathfinding.transformers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.collections15.Transformer;
+
+import repast.simphony.space.graph.RepastEdge;
+import repastInterSim.environment.NetworkEdge;
+
+/*
+ * This transformer use a network edge has an attribute that indicates the edge crosses a road to determine the weight of the edge.
+ */
+public class EdgeRoadLinkIDTransformer<T> implements Transformer<RepastEdge<T>,Integer> {
+	
+	// The road link id to return a different weight value for if the edge crosses them
+	private int crossesRoadLinkWeight = 1;
+	private int notCrossesRoadLinkWeight = 0;
+	
+	public EdgeRoadLinkIDTransformer() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public EdgeRoadLinkIDTransformer(int crossesWeight, int notCrossesWeight) {
+		this.crossesRoadLinkWeight = crossesWeight;
+		this.notCrossesRoadLinkWeight = notCrossesWeight;
+	}
+	
+	@Override
+	public Integer transform(RepastEdge<T> edge) {
+		NetworkEdge<T> ne = (NetworkEdge<T>) edge;
+		
+		if (ne.getRoadLink().getPedRLID().contentEquals("")) {
+			return this.notCrossesRoadLinkWeight;
+		}
+		else {
+			return this.crossesRoadLinkWeight;
+		}
+	}
+}
