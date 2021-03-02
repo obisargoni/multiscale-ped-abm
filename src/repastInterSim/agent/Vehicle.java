d809562952a78cc54121c77b065a818300fb9bcc
Obi Sargoni <othompsarg@hotmail.com>
2021-03-02 10:51:46 +0000

    Deleting unused imports

--- src/repastInterSim/agent/Vehicle.java
+++ src/repastInterSim/agent/Vehicle.java
@@ -4,14 +4,10 @@ import java.text.DecimalFormat;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.Point;
 
-import repast.simphony.context.Context;
 import repast.simphony.engine.schedule.ScheduledMethod;
 import repast.simphony.space.gis.Geography;
-import repast.simphony.util.ContextUtils;
 import repastInterSim.environment.OD;
 import repastInterSim.environment.GISFunctions;
 import repastInterSim.environment.RoadLink;
