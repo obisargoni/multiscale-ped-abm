84c8ee5f083d43d441dc2ed489eeac7713e0dd5b
Obi Sargoni <othompsarg@hotmail.com>
2021-03-05 17:25:55 +0000

    Switching to using methods from environment setup class. Tests all pass.

--- src/repastInterSim/tests/VehicleTest.java
+++ src/repastInterSim/tests/VehicleTest.java
@@ -1,194 +1,16 @@
 package repastInterSim.tests;
 
-
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.Point;
 
-import repast.simphony.context.Context;
-import repast.simphony.context.DefaultContext;
-import repast.simphony.context.space.gis.GeographyFactoryFinder;
-import repast.simphony.context.space.graph.NetworkBuilder;
-import repast.simphony.space.gis.Geography;
-import repast.simphony.space.gis.GeographyParameters;
 import repastInterSim.agent.Ped;
 import repastInterSim.agent.Vehicle;
-import repastInterSim.environment.GISFunctions;
-import repastInterSim.environment.Junction;
-import repastInterSim.environment.NetworkEdgeCreator;
-import repastInterSim.environment.OD;
-import repastInterSim.environment.Road;
-import repastInterSim.environment.RoadLink;
-import repastInterSim.environment.SpatialIndexManager;
-import repastInterSim.environment.contexts.JunctionContext;
-import repastInterSim.environment.contexts.PedestrianDestinationContext;
-import repastInterSim.environment.contexts.RoadContext;
-import repastInterSim.environment.contexts.RoadLinkContext;
 import repastInterSim.main.GlobalVars;
 import repastInterSim.main.IO;
-import repastInterSim.main.SpaceBuilder;
 
 class VehicleTest {
-		
-	Context<Object> context = new DefaultContext<Object>();;
-	
-	String testGISDir = ".//data//test_gis_data//";
-	String roadLinkPath = null;
-	String pedestrianRoadsPath = null;
-	String vehicleRoadsPath = null;
-	String pavementLinkPath = null;
-	String pedJPath = null;
-	String serialisedLookupPath = null;
-	
-	void setUpProperties() throws IOException {
-		IO.readProperties();
-	}
-	
-	void setUpObjectGeography() {
-		GeographyParameters<Object> geoParams = new GeographyParameters<Object>();
-		SpaceBuilder.geography = GeographyFactoryFinder.createGeographyFactory(null).createGeography(GlobalVars.CONTEXT_NAMES.MAIN_GEOGRAPHY, context, geoParams);
-		SpaceBuilder.geography.setCRS(GlobalVars.geographyCRSString);
-		context.add(SpaceBuilder.geography);
-	}
-	
-	void setUpRoads() throws Exception {
-		pedestrianRoadsPath = testGISDir + "topographicAreaPedestrian.shp";
-		vehicleRoadsPath = testGISDir + "topographicAreaVehicle.shp";
-		serialisedLookupPath = testGISDir + "road_link_roads_cache.serialised";
-		
-		// Get road geography
-		Context<Road> testRoadContext = new RoadContext();
-		GeographyParameters<Road> GeoParamsRoad = new GeographyParameters<Road>();
-		SpaceBuilder.roadGeography = GeographyFactoryFinder.createGeographyFactory(null).createGeography("testRoadGeography", testRoadContext, GeoParamsRoad);
-		SpaceBuilder.roadGeography.setCRS(GlobalVars.geographyCRSString);
-		
-		// Load vehicle origins and destinations
-		try {
-			GISFunctions.readShapefile(Road.class, vehicleRoadsPath, SpaceBuilder.roadGeography, testRoadContext);
-			GISFunctions.readShapefile(Road.class, pedestrianRoadsPath, SpaceBuilder.roadGeography, testRoadContext);
-		} catch (MalformedURLException | FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		SpatialIndexManager.createIndex(SpaceBuilder.roadGeography, Road.class);
-	}
-	
-	Geography<RoadLink> setUpRoadLinks(String roadLinkFile) throws MalformedURLException, FileNotFoundException {
-		roadLinkPath = testGISDir + roadLinkFile;
-		
-		// Initialise test road link geography and context
-		SpaceBuilder.roadLinkContext = new RoadLinkContext();
-		GeographyParameters<RoadLink> GeoParams = new GeographyParameters<RoadLink>();
-		Geography<RoadLink> rlG = GeographyFactoryFinder.createGeographyFactory(null).createGeography("orRoadLinkGeography", SpaceBuilder.roadLinkContext, GeoParams);
-		rlG.setCRS(GlobalVars.geographyCRSString);
-				
-		GISFunctions.readShapefile(RoadLink.class, roadLinkPath, rlG, SpaceBuilder.roadLinkContext);
-		SpatialIndexManager.createIndex(rlG, RoadLink.class);
-		
-		return rlG;
-	}
-	
-	void setUpITNRoadLinkGeography() throws Exception {
-		SpaceBuilder.roadLinkGeography = setUpRoadLinks("mastermap-itn RoadLink Intersect Within with orientation.shp");
-	}
-	
-	void setUpORRoadLinkGeography() throws Exception {
-		SpaceBuilder.orRoadLinkGeography = setUpRoadLinks("open-roads RoadLink Intersect Within simplify angles.shp");
-	}
-
-	void setUpODs(String odFile) throws MalformedURLException, FileNotFoundException {
-		
-		// Initialise OD context and geography
-		Context<OD> ODContext = new PedestrianDestinationContext();
-		GeographyParameters<OD> GeoParamsOD = new GeographyParameters<OD>();
-		SpaceBuilder.vehicleDestinationGeography = GeographyFactoryFinder.createGeographyFactory(null).createGeography("testODGeography", ODContext, GeoParamsOD);
-		SpaceBuilder.vehicleDestinationGeography.setCRS(GlobalVars.geographyCRSString);
-		
-		// Load vehicle origins and destinations
-		String testODFile = testGISDir + odFile;
-		GISFunctions.readShapefile(OD.class, testODFile, SpaceBuilder.vehicleDestinationGeography, ODContext);
-		SpatialIndexManager.createIndex(SpaceBuilder.vehicleDestinationGeography, OD.class);
-	}
-	
-	void setUpRoadNetwork(boolean isDirected) {
-		Context<Junction> junctionContext = new JunctionContext();
-		GeographyParameters<Junction> GeoParamsJunc = new GeographyParameters<Junction>();
-		SpaceBuilder.junctionGeography = GeographyFactoryFinder.createGeographyFactory(null).createGeography("junctionGeography", junctionContext, GeoParamsJunc);
-		SpaceBuilder.junctionGeography.setCRS(GlobalVars.geographyCRSString);
-		
-		NetworkBuilder<Junction> builder = new NetworkBuilder<Junction>(GlobalVars.CONTEXT_NAMES.ROAD_NETWORK,junctionContext, isDirected);
-		builder.setEdgeCreator(new NetworkEdgeCreator<Junction>());
-		SpaceBuilder.roadNetwork = builder.buildNetwork();
-		
-		GISFunctions.buildGISRoadNetwork(SpaceBuilder.roadLinkGeography, junctionContext, SpaceBuilder.junctionGeography, SpaceBuilder.roadNetwork);
-	}
-	
-	private Vehicle createVehicle(OD o, OD d) {
-		Vehicle V = new Vehicle(GlobalVars.maxVehicleSpeed, GlobalVars.defaultVehicleAcceleration, GlobalVars.initialVehicleSpeed, o, d);
-		context.add(V);
-		Coordinate oCoord = o.getGeom().getCentroid().getCoordinate();
-		GeometryFactory fac = new GeometryFactory();
-		Point pt = fac.createPoint(oCoord);
-		Geometry vehicleCircle = pt.buffer(2);
-		GISFunctions.moveAgentToGeometry(SpaceBuilder.geography, vehicleCircle, V);
-		V.setLoc();
-		return V;
-	}
-	
-	private Vehicle createVehicle(String oID, String dID) {
-		OD o = null;
-		OD d = null;
-		for (OD od: SpaceBuilder.vehicleDestinationGeography.getAllObjects()) {
-			if (od.getFID().contentEquals(oID)) {
-				o = od;
-			}
-			else if (od.getFID().contentEquals(dID)) {
-				d = od;
-			}
-		}
-		return createVehicle(o,d);
-	}
-	
-	/*
-	 * Code taken from SpaceBuilder that connects Roads with Road Links and visa versa.
-	 */
-	private void assocaiteRoadsWithRoadLinks() {
-		// Link road with itn and OR road links
-		// Also assigns the Road objects to the road links. This enables lookups between OR and ITN road links, through the road objects.
-		for (Road r: SpaceBuilder.roadGeography.getAllObjects()) {
-			List<RoadLink> roadLinks = new ArrayList<RoadLink>();
-			for(RoadLink rl: SpaceBuilder.roadLinkGeography.getAllObjects()) {
-				// Iterating over the vehicle road links (ITN) but using their corresponding ped road link (open road) id to check whether they belong to this vehicle polygon
-				if (rl.getPedRLID().contentEquals(r.getRoadLinkID())) {
-					roadLinks.add(rl);
-					rl.getRoads().add(r);
-				}
-			}
-			
-			RoadLink orLink = null;
-			for(RoadLink rl: SpaceBuilder.orRoadLinkGeography.getAllObjects()) {
-				// Iterating over the vehicle road links (ITN) but using their corresponding ped road link (open road) id to check whether they belong to this vehicle polygon
-				if (rl.getFID().contentEquals(r.getRoadLinkID())) {
-					orLink = rl;
-					orLink.getRoads().add(r);
-					break;
-				}
-			}
-			
-			r.setRoadLinks(roadLinks);
-			r.setORRoadLink(orLink);
-		}
-	}
 	
 	/*
 	 * Test that get crossing pedestrians returns an empty list when there are no pedestrians
@@ -198,14 +20,14 @@ class VehicleTest {
 		// Set up environment
 		try {
 			IO.readProperties();
-			setUpObjectGeography();
-			setUpRoads();
-			setUpITNRoadLinkGeography();
-			setUpORRoadLinkGeography();
-			setUpODs("OD_vehicle_nodes_intersect_within.shp");
-			setUpRoadNetwork(true);
+			EnvironmentSetup.setUpObjectGeography();
+			EnvironmentSetup.setUpRoads();
+			EnvironmentSetup.setUpITNRoadLinks();
+			EnvironmentSetup.setUpORRoadLinks();
+			EnvironmentSetup.setUpVehicleODs("OD_vehicle_nodes_intersect_within.shp");
+			EnvironmentSetup.setUpITNRoadNetwork(true);
 			
-			assocaiteRoadsWithRoadLinks();
+			EnvironmentSetup.assocaiteRoadsWithRoadLinks();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -219,7 +41,7 @@ class VehicleTest {
 		String destID = "osgb4000000029971717";
 		
 		// Create vehicle
-		Vehicle v = createVehicle(originID, destID);
+		Vehicle v = EnvironmentSetup.createVehicle(originID, destID);
 		
 		// Initialise route and current link of vehicle		
 		try {
@@ -239,14 +61,14 @@ class VehicleTest {
 		// Set up environment
 		try {
 			IO.readProperties();
-			setUpObjectGeography();
-			setUpRoads();
-			setUpITNRoadLinkGeography();
-			setUpORRoadLinkGeography();
-			setUpODs("OD_vehicle_nodes_intersect_within.shp");
-			setUpRoadNetwork(true);
+			EnvironmentSetup.setUpObjectGeography();
+			EnvironmentSetup.setUpRoads();
+			EnvironmentSetup.setUpITNRoadLinks();
+			EnvironmentSetup.setUpORRoadLinks();
+			EnvironmentSetup.setUpVehicleODs("OD_vehicle_nodes_intersect_within.shp");
+			EnvironmentSetup.setUpITNRoadNetwork(true);
 			
-			assocaiteRoadsWithRoadLinks();
+			EnvironmentSetup.assocaiteRoadsWithRoadLinks();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -260,7 +82,7 @@ class VehicleTest {
 		String destID = "osgb4000000029971717";
 		
 		// Create vehicle
-		Vehicle v = createVehicle(originID, destID);
+		Vehicle v = EnvironmentSetup.createVehicle(originID, destID);
 		
 		// Initialise route and current link of vehicle		
 		try {
@@ -298,14 +120,14 @@ class VehicleTest {
 		// Set up environment
 		try {
 			IO.readProperties();
-			setUpObjectGeography();
-			setUpRoads();
-			setUpITNRoadLinkGeography();
-			setUpORRoadLinkGeography();
-			setUpODs("OD_vehicle_nodes_intersect_within.shp");
-			setUpRoadNetwork(true);
+			EnvironmentSetup.setUpObjectGeography();
+			EnvironmentSetup.setUpRoads();
+			EnvironmentSetup.setUpITNRoadLinks();
+			EnvironmentSetup.setUpORRoadLinks();
+			EnvironmentSetup.setUpVehicleODs("OD_vehicle_nodes_intersect_within.shp");
+			EnvironmentSetup.setUpITNRoadNetwork(true);
 			
-			assocaiteRoadsWithRoadLinks();
+			EnvironmentSetup.assocaiteRoadsWithRoadLinks();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -319,7 +141,7 @@ class VehicleTest {
 		String destID = "osgb4000000029971717";
 		
 		// Create leader vehicle and step forward for some ticks
-		Vehicle vLeader = createVehicle(originID, destID);
+		Vehicle vLeader = EnvironmentSetup.createVehicle(originID, destID);
 		
 		// Step the leader vehicle ahead a few ticks
 		for (int i=0; i<5; i++) {
@@ -332,7 +154,7 @@ class VehicleTest {
 		}
 		
 		// Now add follower vehicle
-		Vehicle vFollower = createVehicle(originID, destID);
+		Vehicle vFollower = EnvironmentSetup.createVehicle(originID, destID);
 		
 		try {
 			vFollower.getRoute().setRoute();
@@ -389,14 +211,14 @@ class VehicleTest {
 		// Set up environment
 		try {
 			IO.readProperties();
-			setUpObjectGeography();
-			setUpRoads();
-			setUpITNRoadLinkGeography();
-			setUpORRoadLinkGeography();
-			setUpODs("OD_vehicle_nodes_intersect_within.shp");
-			setUpRoadNetwork(true);
+			EnvironmentSetup.setUpObjectGeography();
+			EnvironmentSetup.setUpRoads();
+			EnvironmentSetup.setUpITNRoadLinks();
+			EnvironmentSetup.setUpORRoadLinks();
+			EnvironmentSetup.setUpVehicleODs("OD_vehicle_nodes_intersect_within.shp");
+			EnvironmentSetup.setUpITNRoadNetwork(true);
 			
-			assocaiteRoadsWithRoadLinks();
+			EnvironmentSetup.assocaiteRoadsWithRoadLinks();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -410,7 +232,7 @@ class VehicleTest {
 		String destID = "osgb4000000029971717";
 		
 		// Create leader vehicle and step forward for some ticks
-		Vehicle vLeader = createVehicle(originID, destID);
+		Vehicle vLeader = EnvironmentSetup.createVehicle(originID, destID);
 		
 		// Step once to initialise route
 		try {
@@ -432,7 +254,7 @@ class VehicleTest {
 		assert vLeader.getRoute().getRoadsX().get(0).getFID().contentEquals(currentRLID) == false;
 		
 		// Now add follower vehicle
-		Vehicle vFollower = createVehicle(originID, destID);
+		Vehicle vFollower = EnvironmentSetup.createVehicle(originID, destID);
 		
 		try {
 			vFollower.getRoute().setRoute();
@@ -528,10 +350,10 @@ class VehicleTest {
 		}
 		
 		// Create pedestrian that will cross first road link
-		Ped pedMinDist = createPedestrian(3,4,false);
+		Ped pedMinDist = EnvironmentSetup.createPedestrian(3,4,false);
 		
 		// Create a vehicle that moves along same link as pedestrian
-		Vehicle v = createVehicle("osgb4000000029970447", "osgb4000000029970446");
+		Vehicle v = EnvironmentSetup.createVehicle("osgb4000000029970447", "osgb4000000029970446");
 		
 		// Step the vehicle and pedestrian once to initialise them
 		try {
@@ -618,10 +440,10 @@ class VehicleTest {
 		}
 		
 		// Create pedestrian that will cross first road link
-		Ped pedMinDist = createPedestrian(3,4,false);
+		Ped pedMinDist = EnvironmentSetup.createPedestrian(3,4,false);
 		
 		// Create a vehicle that moves along same link as pedestrian
-		Vehicle v = createVehicle("osgb4000000029970447", "osgb4000000029970446");
+		Vehicle v = EnvironmentSetup.createVehicle("osgb4000000029970447", "osgb4000000029970446");
 		
 		// Step the vehicle and pedestrian once to initialise them
 		try {
