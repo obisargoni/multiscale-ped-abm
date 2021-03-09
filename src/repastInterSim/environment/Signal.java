6f7bb6b576e77295867ecc3786aaaf657970ec72
Obi Sargoni <othompsarg@hotmail.com>
2021-03-09 18:16:02 +0000

    Changing how signal class records and keeps track of state and phases. Instead of hash map lookup use arrays and access via indexes. states and phases described using chars ratehr than booleans, allows for more flexibility in future.

--- src/repastInterSim/environment/Signal.java
+++ src/repastInterSim/environment/Signal.java
@@ -1,17 +1,19 @@
 package repastInterSim.environment;
 
-import java.util.HashMap;
-
 import repast.simphony.engine.schedule.ScheduledMethod;
 
 public class Signal {
 	
 	protected String signalID; // The id of the road link this road object should be linked with
 	
-	// The state of the signal will indicate which road links cars can progress on
-	protected HashMap<String, Boolean> state = new HashMap<String, Boolean>();;
+	// The itn road links this signal controls
+	protected String[] itnLinkIDs;
 	
-	protected HashMap<String, Boolean>[] phases;
+	// The state of the signal will indicate which road links cars can progress on
+	protected char[] state;
+	
+	// Nested array, each elements contains a phase state
+	protected char[][] phases;
 	protected int[] phaseDurations; 
 	protected int phaseIndex;
 	
@@ -23,19 +25,20 @@ public class Signal {
 	public Signal() {
 	}
 	
-	public Signal(HashMap<String, Boolean>[] p, int[] pD, int pI) {
-		init(p, pD, pI);
+	public Signal(String[] rlIDs, char[][] phs, int[] pD, int pI) {
+		init(rlIDs, phs, pD, pI);
 	}
 	
-	public Signal(HashMap<String, Boolean>[] p, int[] pD) {
-		init(p, pD, 0);
+	public Signal(String[] rlIDs, char[][] phs, int[] pD) {
+		init(rlIDs, phs, pD, 0);
 	}
 	
-	private void init(HashMap<String, Boolean>[] p, int[] pD, int pI) {
-		this.phases = p;
+	private void init(String[] rlIDs, char[][] phs, int[] pD, int pI) {
+		this.itnLinkIDs = rlIDs;
+		this.phases = phs;
 		this.phaseDurations = pD;
 		this.phaseIndex = pI;
-		setState(p[this.phaseIndex]);
+		setState(phs[this.phaseIndex]);
 	}
 		
 	/*
@@ -44,41 +47,29 @@ public class Signal {
 	 * @param String rlID
 	 * 		The road link ID to get the signal state for
 	 * 
-	 * @return Boolean. The state of the signal
+	 * @return char. The state of the signal
 	 */
-	public boolean getState(String rlID) {
-		return this.state.get(rlID);
+	public char getState(String rlID) {
+		// get index this road link appears at and return state value at this index
+		for(int i=0; i<this.itnLinkIDs.length; i++) {
+			if(this.itnLinkIDs[i].contentEquals(rlID)) {
+				return this.state[i];
+			}
+		}
+		// return u for unknown if can't match road link id
+		return 'u';
 	}
 	
 	/*
 	 * Set the state of the Signal.
 	 * 
-	 * @param HashMap<String, Boolean> s
-	 * 		The state to set the signal to. Contains lookup from road link IDs to indicator of whether vehicles on this road links can progress 
+	 * @param char[[ s
+	 * 		The state to set the signal to. Array of characters, each character refers to the state the signal assigns a road link. 
 	 * when the reach the signal.
 	 */
-	public void setState(HashMap<String, Boolean> s) {
+	public void setState(char[] s) {
 		this.state = s;
 	}
-	
-	/*
-	 * Reverse the state of the signal.
-	 */
-	public void switchState() {
-		this.state.replaceAll((k,v) -> !v);
-	}
-	
-	public void switchState(String rlID) {
-		Boolean switched = !this.state.get(rlID);
-		this.state.replace(rlID, switched);
-	}
-	
-	public void switchState(String[] rlIDs) {
-		for(int i=0; i< rlIDs.length; i++) {
-			this.switchState(rlIDs[i]);
-		}
-	}
-	
 	
 	/*
 	 * Change the current state of the signal to be the next phase when the current phases duration has ended.
