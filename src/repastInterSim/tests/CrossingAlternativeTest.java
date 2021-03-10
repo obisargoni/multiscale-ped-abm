891843664b2e41ab4b7df5c0edc44b583f3cb2d8
Obi Sargoni <othompsarg@hotmail.com>
2021-03-10 12:48:48 +0000

    Adding test of changing of signal phases

--- src/repastInterSim/tests/CrossingAlternativeTest.java
+++ src/repastInterSim/tests/CrossingAlternativeTest.java
@@ -57,5 +57,92 @@ class CrossingAlternativeTest {
 		assert ca1.getState("osgb4000000030238818") == 'r';
 		assert ca1.getState("wrong_link_id") == 'u';
 	}
+	
+	@Test
+	void testSignalPhaseChange() {
+		try {
+			EnvironmentSetup.setUpCrossingAlternatives("CrossingAlternatives.shp");
+		} catch (MalformedURLException | FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assert SpaceBuilder.caGeography != null;
+		assert SpaceBuilder.caGeography.size() == 3;
+		
+		CrossingAlternative ca1030 = null;
+		CrossingAlternative ca530a = null;
+		CrossingAlternative ca530b = null;
+		for (CrossingAlternative ca_: SpaceBuilder.caGeography.getAllObjects()) {
+			if(ca_.getID() == 3) {
+				ca1030 = ca_;
+			}
+			else if (ca_.getID() == 4) {
+				ca530a = ca_;
+			}
+			else if (ca_.getID() == 5) {
+				ca530b = ca_;
+			}
+		}
+		
+		// Check initial states of the signals then check that state changes as expected
+		assert ca1030.getState("osgb4000000030238817") == 'r';
+		assert ca1030.getState("osgb4000000030238818") == 'r';
+		
+		assert ca530a.getState("osgb4000000030238839") == 'r';
+		assert ca530b.getState("osgb4000000030418774") == 'r';
+		
+		// After 5 ie on the 6th tick) ticks two of the signals should change to green
+		for (int i=0; i<6; i++) {
+			ca1030.step();
+			ca530a.step();
+			ca530b.step();
+		}
+		
+		assert ca1030.getState("osgb4000000030238817") == 'r';
+		assert ca1030.getState("osgb4000000030238818") == 'r';
+		
+		assert ca530a.getState("osgb4000000030238839") == 'g';
+		assert ca530b.getState("osgb4000000030418774") == 'g';
+		
+		// After 5 more ticks the other signal should also be green
+		for (int i=0; i<6; i++) {
+			ca1030.step();
+			ca530a.step();
+			ca530b.step();
+		}
+
+		assert ca1030.getState("osgb4000000030238817") == 'g';
+		assert ca1030.getState("osgb4000000030238818") == 'g';
+		
+		assert ca530a.getState("osgb4000000030238839") == 'g';
+		assert ca530b.getState("osgb4000000030418774") == 'g';
+		
+		// After a further 25 steps 530 signals should revert to red
+		for (int i=0; i<26; i++) {
+			ca1030.step();
+			ca530a.step();
+			ca530b.step();
+		}
+		
+		assert ca1030.getState("osgb4000000030238817") == 'g';
+		assert ca1030.getState("osgb4000000030238818") == 'g';
+		
+		assert ca530a.getState("osgb4000000030238839") == 'r';
+		assert ca530b.getState("osgb4000000030418774") == 'r';
+		
+		// After 5 more ticks 1030 signal should be on red whilst the others are back to green
+		for (int i=0; i<6; i++) {
+			ca1030.step();
+			ca530a.step();
+			ca530b.step();
+		}
+		assert ca1030.getState("osgb4000000030238817") == 'r';
+		assert ca1030.getState("osgb4000000030238818") == 'r';
+		
+		assert ca530a.getState("osgb4000000030238839") == 'g';
+		assert ca530b.getState("osgb4000000030418774") == 'g';
+		
+	}
 
 }
