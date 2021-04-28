eba0705ffabc44d0b4f15af579d156adf7ec2b98
Obi Sargoni <othompsarg@hotmail.com>
2021-04-28 09:14:52 +0100

    New version of pedestrianODFlows script which selects some pedestrian ODsand generates random flows between them.

--- scripts/gis_data_processing/pedestrianODFlows.py
+++ scripts/gis_data_processing/pedestrianODFlows.py
@@ -1,19 +1,64 @@
 import pandas as pd
 import numpy as np
 import os
-import geopandas as gpd 
+import geopandas as gpd
+import json
 
 ##################
 #
 # Config
 #
 ##################
+
+with open("config.json") as f:
+    config = json.load(f)
+
+# Proportion of ITN nodes to use as vehicle ODs
+prop_random_ODs = 0.3
+
 gis_data_dir = config['gis_data_dir']
-output_directory = os.path.join(gis_data_dir, "processed_gis_data")
+processed_gis_dir = os.path.join(gis_data_dir, "processed_gis_data")
 
-ped_ods_file = os.path.join(output_directory, config['pedestrian_od_file'])
-output_file = os.path.join(output_directory, config["pedestrianODFlows.csv"])
+pavement_nodes_file = os.path.join(processed_gis_dir, config["pavement_nodes_file"])
 
+pedestrian_od_flows = os.path.join(processed_gis_dir, config['pedestrian_od_flows'])
+pedestrian_od_file = os.path.join(processed_gis_dir, config['pedestrian_od_file'])
+
+poi_file = os.path.join(gis_data_dir, config["poi_file"])
+centre_poi_ref = config["centre_poi_ref"]
+dist_from_centre_threshold = 50
+
+#################
+#
+# Load data and select pedestrian ODs
+#
+#################
+
+# Select Origin pavement nodes based on POIs
+gdf_pois = gpd.read_file(poi_file)
+gdf_pave_node = gpd.read_file(pavement_nodes_file)
+
+centre_poi_geom = gdf_pois.loc[ gdf_pois['ref_no'] == centre_poi_ref, 'geometry'].values[0]
+
+gdf_pave_node['dist_to_centre'] = gdf_pave_node['geometry'].distance(centre_poi_geom)
+
+Os = gdf_pave_node.sort_values(by = 'dist_to_centre', ascending = True)['fid'].values[0:1]
+
+# Select destination nodes randomly
+candidates = gdf_pave_node.loc[ gdf_pave_node['dist_to_centre'] > dist_from_centre_threshold, 'fid'].values
+
+nDs = int(prop_random_ODs * len(candidates))
+
+Ds = np.random.choice(candidates, nDs)
+
+ODs = np.concatenate([Os, Ds])
+
+gdfODs = gdf_pave_node.loc[ gdf_pave_node['fid'].isin(ODs), ['fid', 'geometry']]
+
+gdfODs.to_file(pedestrian_od_file)
+
+# Load ped ODs to get number of origins/destinations
+gdfODs = gpd.read_file(pedestrian_od_file)
 
 #################
 #
@@ -21,18 +66,21 @@ output_file = os.path.join(output_directory, config["pedestrianODFlows.csv"])
 #
 #################
 
-# Load ped ODs to get number of origins/destinations
-gdf_od = gpd.read_file(ped_ods_file)
+# Initialise OD flows matrix
+flows = np.zeros([len(ODs), len(ODs)])
+dfFlows = pd.DataFrame(flows, columns = ODs, index = ODs)
 
-n_origins = gdf_od['id'].unique().shape[0]
-n_destinations = n_origins
+# Set random flows between origins and destinations
+for o in Os:
+	for d in Ds:
+		if (o == d):
+			continue
 
-random_flows = np.random.rand(n_origins,n_destinations)
-df1 = pd.DataFrame(random_flows)
+		f_ij = np.random.rand()
+		dfFlows.loc[o, d] = f_ij
 
-# Set diagonal elements to 0, don't want an agent's destination to be the same as its origin
-for i in range(max(n_origins,n_destinations)):
-	df1.loc[i,i] = 0
+		f_ji = np.random.rand()
+		dfFlows.loc[d, o] = f_ji
 
-df1.columns = gdf_od['id']
-df1.to_csv(output_file, index=False, header=True)
+
+dfFlows.to_csv(pedestrian_od_flows, index=False)
