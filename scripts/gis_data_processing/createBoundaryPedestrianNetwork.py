bb45daa1e77fa0a8ddda7b7495c58060bee47f73
Obi Thompson Sargoni <33906525+obisargoni@users.noreply.github.com>
2021-05-10 14:02:36 +0100

    Converting tabs to spaces. Also using method for getting ped network nodes.

--- scripts/gis_data_processing/createBoundaryPedestrianNetwork.py
+++ scripts/gis_data_processing/createBoundaryPedestrianNetwork.py
@@ -1,5 +1,5 @@
 # Script for producing the pedestrian network using a road network and walkable bounday
-
+import sys
 import json
 import numpy as np
 import pandas as pd
@@ -43,7 +43,7 @@ gdfTopoPed = gpd.read_file(os.path.join(output_directory, config["topo_pedestria
 gdfTopoVeh.crs = projectCRS
 gdfTopoPed.crs = projectCRS
 
-# Load boundary data - used to identify traffic island pedestrian polygons
+# Load boundary data - used to identify pavement nodes
 gdfBoundary = gpd.read_file(os.path.join(output_directory, config["boundary_file"]))
 gdfBoundary.crs = projectCRS
 
@@ -60,122 +60,135 @@ output_ped_links_file = os.path.join(output_directory, config["pavement_links_fi
 #################################
 def road_node_pedestrian_nodes_metadata(graph, road_node_id):
 
-	# Method for getting ped nodes for a single junctions
-	edges = graph.edges(road_node_id, data=True)
+    # Method for getting ped nodes for a single junctions
+    edges = graph.edges(road_node_id, data=True)
 
-	dfPedNodes = pd.DataFrame()
+    dfPedNodes = pd.DataFrame()
 
-	# Iterate over pairs of road link IDs in order to find the pedestrian nodes that lie between these two road links
-	for e1, e2 in itertools.combinations(edges, 2):
-		# Initialise data to go into the geodataframe
-		row = {"juncNodeID":road_node_id, "rlID1":e1[-1]['fid'], "rlID2":e2[-1]['fid']}
+    # Iterate over pairs of road link IDs in order to find the pedestrian nodes that lie between these two road links
+    for e1, e2 in itertools.combinations(edges, 2):
+        # Initialise data to go into the geodataframe
+        row = {"juncNodeID":road_node_id, "rlID1":e1[-1]['fid'], "rlID2":e2[-1]['fid']}
 
-		dfPedNodes = dfPedNodes.append(row, ignore_index = True)
-		
-	# Filter dataframe to exclude empty geometries
-	return dfPedNodes
+        dfPedNodes = dfPedNodes.append(row, ignore_index = True)
+        
+    # Filter dataframe to exclude empty geometries
+    return dfPedNodes
 
 def multiple_road_node_pedestrian_nodes_metadata(graph, road_node_ids):
-	dfPedNodes = pd.DataFrame()
+    dfPedNodes = pd.DataFrame()
 
-	for road_node_id in road_node_ids:
-		df = road_node_pedestrian_nodes_metadata(graph, road_node_id)
-		dfPedNodes = pd.concat([dfPedNodes, df])
+    for road_node_id in road_node_ids:
+        df = road_node_pedestrian_nodes_metadata(graph, road_node_id)
+        dfPedNodes = pd.concat([dfPedNodes, df])
 
-	gdfPedNodes.index = np.arange(gdfPedNodes.shape[0])
-	return gdfPedNodes
+    dfPedNodes.index = np.arange(dfPedNodes.shape[0])
+    return dfPedNodes
 
-def assign_boundary_coordinates_to_ped_nodes(dfPN, gdfRoadLinks, gdfBounds):
-	"""Identify coordinates for ped nodes based on the bounday.
-	"""
+def assign_boundary_coordinates_to_ped_nodes(dfPN, gdfRoadLinks, serBounds, crs = projectCRS):
+    """Identify coordinates for ped nodes based on the bounday.
+    """
 
-	# Loop through nodes, get corresponding road links and the boundary between them
-	for ix, row in dfPN.iterrows():
+    # Initialise output
+    gdfPN = gpd.GeoDataFrame(dfPN)
+    gdfPN['geometry'] = None
+    gdfPN.set_geometry('geometry', inplace=True, crs = crs)
 
-		rlID1 = row['rlID1']
-		rlID2 = row['rlID2']
+    # Loop through nodes, get corresponding road links and the boundary between them
+    for ix, row in gdfPN.iterrows():
 
-		rlg1 = gdfRoadLinks.loc[ gdfRoadLinks['fid'] == rlID1]
-		rlg2 = gdfRoadLinks.loc[ gdfRoadLinks['fid'] == rlID2]
+        rlID1 = row['rlID1']
+        rlID2 = row['rlID2']
 
-		road_node = rlg1.intersection(rlg2)
-		assert isinstance(road_node, Point)
-		road_node = np.array(road_node.coords)
+        rlg1 = gdfRoadLinks.loc[ gdfRoadLinks['fid'] == rlID1, 'geometry'].values[0]
+        rlg2 = gdfRoadLinks.loc[ gdfRoadLinks['fid'] == rlID2, 'geometry'].values[0]
 
-		if road_node.coords == rlg1.coords[0]:
-			rlp1 = np.array(rlg1.coords[-1])
-		else:
-			rlp1 = np.array(rlg1.coords[0])
+        road_node = rlg1.intersection(rlg2)
+        if isinstance(road_node, Point)==False:
+            print("Non-point intersection. {} {} {}".format(ix, rlID1, rlID2))
+            print(road_node.type)
+            print("\n") 
+            continue
+        else:
+            pass
 
-		if road_node.coords == rlg2.coords[0]:
-			rlp2 = np.array(rlg2.coords[-1])
-		else:
-			rlp2 = np.array(rlg2.coords[0])
+        if road_node.coords[0] == rlg1.coords[0]:
+            rlp1 = np.array(rlg1.coords[-1])
+        else:
+            rlp1 = np.array(rlg1.coords[0])
 
-		v1 = rlp1 - road_node
-		v2 = rlp2 - road_node
+        if road_node.coords[0] == rlg2.coords[0]:
+            rlp2 = np.array(rlg2.coords[-1])
+        else:
+            rlp2 = np.array(rlg2.coords[0])
 
-		# Find boundary between using ray casting
-		a1 = angle_between_north_and_vector(v1)
-		a2 = angle_between_north_and_vector(v2)
+        road_node = np.array(road_node.coords[0])
+        v1 = rlp1 - road_node
+        v2 = rlp2 - road_node
 
-		intersecting_boundary_geom_ids = np.array([])
-		for l in rays_between_angles(a1, a2):
-			ids = gdfBounds.loc[ gdfBounds.intersects(l)].index
-			intersecting_boundary_geom_ids = np.concat([intersecting_boundary_geom_ids, ids])
+        # Find boundary between using ray casting
+        a1 = angle_between_north_and_vector(v1)
+        a2 = angle_between_north_and_vector(v2)
 
-
-		# Now find nearest boundary coordinate from intersecting boundaries
-		min_dist = sys.maxsize
-		ped_node_geom = None
-		for row_id in intersecting_boundary_geom_ids:
-			geom = gdfBounds.loc[row_id, 'geometry']
-
-			for c in geom.coords:
-				rn = Point(road_node)
-				p = Point(c)
-				d = rn.distance(p)
-				if d < min_dist:
-					min_dist = d
-					ped_node_geom = p
-
-		row['geometry'] = ped_node_geom
+        intersecting_boundary_geom_ids = np.array([])
+        for l in rays_between_angles(a1, a2, road_node):
+            ids = serBounds.loc[ serBounds.intersects(l)].index
+            intersecting_boundary_geom_ids = np.concatenate([intersecting_boundary_geom_ids, ids])
 
 
-def rays_between_angles(a1, a2, sample_res = 10, ray_length = 50):
-	sample_res = (2*np.pi) * (sample_res/360.0) # 10 degrees in rad
-	sampled_angles = range(a1,a2,sample_res)
-	for sa in sampled_angles:
-		p1 = Point(road_node)
-		p2 = Point([p1.x + ray_length*np.sin(sa), p1.y + ray_length*np.cos(sa)])
-		l = LineString([p1,p2])
-		yield l
+        # Now find nearest boundary coordinate from intersecting boundaries
+        min_dist = sys.maxsize
+        ped_node_geom = None
+        for row_id in set(intersecting_boundary_geom_ids):
+            geom = serBounds.loc[row_id]
+
+            for c in geom.exterior.coords:
+                rn = Point(road_node)
+                p = Point(c)
+                d = rn.distance(p)
+                if d < min_dist:
+                    min_dist = d
+                    ped_node_geom = p
+
+        row['geometry'] = ped_node_geom
+
+    return gdfPN
+
+
+def rays_between_angles(a1, a2, start_coord, sample_res = 10, ray_length = 50):
+    sample_res = (2*np.pi) * (sample_res/360.0) # 10 degrees in rad
+    sampled_angles = np.arange(min(a1,a2), max(a1,a2),sample_res)
+    for sa in sampled_angles:
+        p1 = Point(start_coord)
+        p2 = Point([p1.x + ray_length*np.sin(sa), p1.y + ray_length*np.cos(sa)])
+        l = LineString([p1,p2])
+        yield l
 
 def unit_vector(v):
-	magV = np.linalg.norm(v)
-	return v / magV
+    magV = np.linalg.norm(v)
+    return v / magV
 
 def angle_between_north_and_unit_vector(u):
-	n = (0,1)
+    n = (0,1)
 
-	signX = np.sign(u[0])
-	signY = np.sign(u[1])
+    signX = np.sign(u[0])
+    signY = np.sign(u[1])
 
-	dp = np.dot(n, u)
-	a = np.acos(dp)
+    dp = np.dot(n, u)
+    a = np.arccos(dp)
 
     # Dot product gives angle between vectors. We need angle clockwise from north
     if (signX == 1):
-    	return a
+        return a
     elif (signX == -1):
-    	return 2*np.pi - a
+        return 2*np.pi - a
     else:
-    	return a
+        return a
 
 def angle_between_north_and_vector(v):
- 	unit_v = unit_vector(v)
- 	return angle_between_north_and_unit_vector(unit_v)
-			
+    unit_v = unit_vector(v)
+    return angle_between_north_and_unit_vector(unit_v)
+            
 
 def ang(lineA, lineB):
     # Get nicer vector form
@@ -191,7 +204,7 @@ def ang(lineA, lineB):
     # Get cosine value
     cos_ = round(dot_prod/magA/magB, 4)
     # Get angle in radians and then convert to degrees
-    angle = math.acos(cos_)
+    angle = math.arccos(cos_)
     # Basically doing angle <- angle mod 360
     ang_deg = math.degrees(angle)%360
 
@@ -199,7 +212,6 @@ def ang(lineA, lineB):
         # As in if statement
         return 360 - ang_deg
     else: 
-
         return ang_deg
 ################################
 #
@@ -216,6 +228,9 @@ gdfORLink['fid_dict'] = gdfORLink['fid'].map(lambda x: {"fid":x})
 edges = gdfORLink.loc[:,['MNodeFID','PNodeFID', 'fid_dict']].to_records(index=False)
 G.add_edges_from(edges)
 
+node_degrees = G.degree()
+df_node_degree = pd.DataFrame(node_degrees)
+df_node_degree.columns = ['nodeID', 'nodeDegree']
 
 # Node metadata
 dfPedNodes = multiple_road_node_pedestrian_nodes_metadata(G, df_node_degree['nodeID'].values)
@@ -231,11 +246,15 @@ dfPedNodes = multiple_road_node_pedestrian_nodes_metadata(G, df_node_degree['nod
 #
 ##################################
 
-gdfPedNodes = assign_boundary_coordinates_to_ped_nodes(dfPedNodes, gdfORLink, gdfBoundary)
+# Buffer the boundary so that nodes are located slightly away from walls
+gdfBoundaryBuff = gdfBoundary.buffer(0.5)
+
+gdfPedNodes = assign_boundary_coordinates_to_ped_nodes(dfPedNodes, gdfORLink, gdfBoundaryBuff)
 
 # alternatively could have assign_ped_veh_polygon_coordaintes_to_ped_nodes()
 
 # Remove the multipoints - seems like these are traffic islands - might want to think about including in future
+'''
 gdfPedNodes = gdfPedNodes.loc[ gdfPedNodes['geometry'].type != 'MultiPoint']
 
 # Create id for each ped node
@@ -243,10 +262,4 @@ gdfPedNodes['fid'] = ['pave_node_{}'.format(i) for i in gdfPedNodes.index]
 gdfPedNodes.crs = projectCRS
 
 gdfPedNodes.to_file(output_ped_nodes_file)
-
-
-
-
-
-
-
+'''
