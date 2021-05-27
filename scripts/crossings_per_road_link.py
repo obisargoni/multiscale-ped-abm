14a89662b5b9494159c192d691ff5d240e50585a
Obi Thompson Sargoni <33906525+obisargoni@users.noreply.github.com>
2021-05-27 11:45:14 +0100

    Reformating script for clarity

--- scripts/crossings_per_road_link.py
+++ scripts/crossings_per_road_link.py
@@ -15,6 +15,7 @@ import batch_data_utils as bd_utils
 # Globals
 #
 #####################################
+project_crs = {'init': 'epsg:27700'}
 
 with open(".//gis_data_processing//config.json") as f:
     config = json.load(f)
@@ -29,36 +30,51 @@ or_links_file = os.path.join(gis_data_dir, config['openroads_link_processed_file
 or_nodes_file = os.path.join(gis_data_dir, config['openroads_node_processed_file'])
 
 
+# Model output data
+output_data_dir = "..\\output\\processed_data\\"
+file_datetime_string = "2020.Feb.20.14_58_33"
+file_datetime  =dt.strptime(file_datetime_string, "%Y.%b.%d.%H_%M_%S")
+file_re = bd_utils.get_file_regex("pedestrian_pave_link_crossings")
+ped_crossings_file = os.path.join(data_dir, bd_utils.most_recent_directory_file(data_dir, file_re))
+
 # Output paths
 img_dir = "..\\output\\img\\"
 crossing_network_fig = os.path.join(img_dir, "crossing_network.png")
 
-output_data_dir = "..\\output\\processed_data\\"
 
-project_crs = {'init': 'epsg:27700'}
+#####################################
+#
+#
+# Load Data
+#
+#
+#####################################
 
-file_datetime_string = "2020.Feb.20.14_58_33"
-file_datetime  =dt.strptime(file_datetime_string, "%Y.%b.%d.%H_%M_%S")
-
-# Load batch result data and gis data
-file_re = bd_utils.get_file_regex("pedestrian_pave_link_crossings")
-ped_crossings_file = os.path.join(data_dir, bd_utils.most_recent_directory_file(data_dir, file_re))
-
+# Data from model run
 dfPedCrossings = pd.read_csv(ped_crossings_file)
 
+# GIS Data
 gdfPaveNetwork = gpd.read_file(pavement_links_file)
 gdfORLinks = gpd.read_file(or_links_file)
 gdfORNodes = gpd.read_file(or_nodes_file)
 
 
+
+#######################################
+#
+#
+# Process data
+#
+#
+########################################
+
 # Filter to just include paveent links used for crossing
 dfPedCrossings = dfPedCrossings.loc[ ~dfPedCrossings['TraversedPavementLinkID'].isnull()]
-
 dfPedCrossings = dfPedCrossings.merge(gdfPaveNetwork, left_on = 'TraversedPavementLinkID', right_on = 'fid', how = 'left', indicator=True)
 assert dfPedCrossings.loc[ dfPedCrossings['_merge']!='both'].shape[0]==0
 dfPedCrossings.drop('_merge', axis=1, inplace=True)
 
-# Aggregate
+# Aggregate crossing counts
 dfCrossingCounts = dfPedCrossings.groupby(['run', 'pedRLID']).apply(lambda g: g.shape[0]).reset_index()
 dfCrossingCounts.rename(columns = {0:'cross_count'}, inplace=True)
 
@@ -66,6 +82,15 @@ dfCrossingCounts.rename(columns = {0:'cross_count'}, inplace=True)
 gdfCrossingCounts = pd.merge(gdfORLinks, dfCrossingCounts, left_on = 'fid', right_on = 'pedRLID', how = 'left')
 gdfCrossingCounts['cross_count'] = gdfCrossingCounts['cross_count'].fillna(0)
 
+
+
+##########################################
+#
+#
+# Plot network
+#
+#
+##########################################
 
 # Plot
 import networkx as nx
