9c87b0c7b21f6dfafd3807e973a6d89503200015
Obi Thompson Sargoni <33906525+obisargoni@users.noreply.github.com>
2021-06-10 18:00:47 +0100

    Corrections to processing of road crossing data. Need to drop duplicated entries and check that data used for visualisations only has one pavement link per ped per run

--- scripts/crossings_per_road_link.py
+++ scripts/crossings_per_road_link.py
@@ -8,6 +8,7 @@ import geopandas as gpd
 import re
 import networkx as nx
 from datetime import datetime as dt
+from shapely.geometry import Point
 
 import batch_data_utils as bd_utils
 
@@ -23,7 +24,7 @@ with open(".//gis_data_processing//config.json") as f:
 
 gis_data_dir = os.path.abspath("..\\data\\model_gis_data")
 data_dir = config['batch_data_dir']
-img_dir = ".\\output\\img\\"
+img_dir = "..\\output\\img\\"
 l_re = re.compile(r"(\d+\.\d+),\s(\d+\.\d+)")
 
 pavement_links_file = os.path.join(gis_data_dir, config['pavement_links_file'])
@@ -32,17 +33,13 @@ or_nodes_file = os.path.join(gis_data_dir, config['openroads_node_processed_file
 
 
 # Model output data
-file_datetime_string = "2021.May.28.17_57_01"
+file_datetime_string = "2021.Jun.04.12_48_52"
 file_datetime  =dt.strptime(file_datetime_string, "%Y.%b.%d.%H_%M_%S")
-file_re = bd_utils.get_file_regex("pedestrian_pave_link_crossings", file_datetime = None)
+file_re = bd_utils.get_file_regex("pedestrian_pave_link_crossings", file_datetime = file_datetime)
 ped_crossings_file = os.path.join(data_dir, bd_utils.most_recent_directory_file(data_dir, file_re))
 
-file_re = bd_utils.get_file_regex("pedestrian_pave_link_crossings", file_datetime = None, suffix = 'batch_param_map')
+file_re = bd_utils.get_file_regex("pedestrian_pave_link_crossings", file_datetime = file_datetime, suffix = 'batch_param_map')
 batch_file = bd_utils.most_recent_directory_file(data_dir, file_re)
-
-# Output paths
-img_dir = "..\\output\\img\\"
-crossing_network_fig = os.path.join(img_dir, "crossing_network.png")
 
 
 #####################################
@@ -82,25 +79,39 @@ dict_node_pos = dict(zip(points_pos.index, node_posistions))
 #
 ########################################
 
-# Filter to just include pavement links used for crossing
-dfPedCrossings = dfPedCrossings.loc[ ~dfPedCrossings['CurrentPavementLinkID'].isnull()]
-dfPedCrossings = dfPedCrossings.merge(gdfPaveNetwork, left_on = 'CurrentPavementLinkID', right_on = 'fid', how = 'left', indicator=True)
-assert dfPedCrossings.loc[ dfPedCrossings['_merge']!='both'].shape[0]==0
-dfPedCrossings.drop('_merge', axis=1, inplace=True)
-dfPedCrossings['FullStrategicPathString'] = dfPedCrossings['FullStrategicPathString'].map(lambda s: s.strip('-').split('-'))
+# Process raw model output to 
+# - just include pavement links used for crossing and with a crossing coordinate and a crossing type
+# - Drop duplicates, expect just one crossing per ped per pavement link - can't include crossing coords string bc sometimes it includes both crossing coords
+# - Check that there is one crossing per link per ped
+dfPedCrossings = dfPedCrossings.loc[(~dfPedCrossings['CurrentPavementLinkID'].isnull())]
+
+dfPedCrossings = dfPedCrossings.drop_duplicates(subset = ['run', 'ID', 'ChosenCrossingTypeString', 'CurrentPavementLinkID'])
+
+# Need to account for cases where two crossing types recorded for a pavement link.
+
+cross_per_ped_link = dfPedCrossings.groupby(['run', 'ID', 'CurrentPavementLinkID']).apply(lambda df: df.shape[0])
+#assert cross_per_ped_link.loc[ cross_per_ped_link!=1].shape[0]==0
+
+# Now combine with pavement network data to aggregate crossings per OR link
+
+# Merge with pavement network to find the OR road link being crossed
+dfPedCrossingsLinks = dfPedCrossings.merge(gdfPaveNetwork, left_on = 'CurrentPavementLinkID', right_on = 'fid', how = 'left', indicator=True)
+assert dfPedCrossingsLinks.loc[ dfPedCrossingsLinks['_merge']!='both'].shape[0]==0
+dfPedCrossingsLinks.drop('_merge', axis=1, inplace=True)
+dfPedCrossingsLinks['FullStrategicPathString'] = dfPedCrossingsLinks['FullStrategicPathString'].map(lambda s: s.strip('-').split('-'))
 
 # Aggregate crossing counts
-dfCrossingCounts = dfPedCrossings.groupby(['run', 'pedRLID']).apply(lambda g: g.shape[0]).reset_index()
+dfCrossingCounts = dfPedCrossingsLinks.groupby(['run', 'pedRLID']).apply(lambda g: g.shape[0]).reset_index()
 dfCrossingCounts.rename(columns = {0:'cross_count'}, inplace=True)
 
 # Aggregate unmarked crossing counts
-dfUmCC = dfPedCrossings.loc[dfPedCrossings['ChosenCrossingTypeString']=='unmarked'].groupby(['run', 'pedRLID']).apply(lambda g: g.shape[0]).reset_index()
+dfUmCC = dfPedCrossingsLinks.loc[dfPedCrossingsLinks['ChosenCrossingTypeString']=='unmarked'].groupby(['run', 'pedRLID']).apply(lambda g: g.shape[0]).reset_index()
 dfUmCC.rename(columns = {0:'um_cross_count'}, inplace=True)
 dfCrossingCounts = pd.merge(dfCrossingCounts, dfUmCC, on = ['run', 'pedRLID'], how = 'left')
 
 # Get number of pedestrians per road link, use this to normalise crossing counts - crossings per ped on link
 # Given fixed ODs and flows should get same number of peds per road link but different numbers crossing.
-road_links = pd.Series(np.concatenate(dfPedCrossings['FullStrategicPathString'].values))
+road_links = pd.Series(np.concatenate(dfPedCrossingsLinks['FullStrategicPathString'].values))
 peds_per_link = road_links.value_counts()
 peds_per_link.name = 'peds_per_link'
 
@@ -121,6 +132,27 @@ gdfCrossingCounts['cmap_value'] = gdfCrossingCounts['cross_count_pp'].map(lambda
 
 um_max_cc_pp = gdfCrossingCounts['um_cross_count_pp'].max()
 gdfCrossingCounts['um_cmap_value'] = gdfCrossingCounts['um_cross_count_pp'].map(lambda c: 255*(c/max_cc_pp))
+
+
+# Also process data to get coordinates of crossing points so these can be mapped
+
+# Filter out records without primary crossings
+dfPedCrossings = dfPedCrossings.loc[    (~dfPedCrossings['CrossingCoordsString'].isnull())  &
+                                        (dfPedCrossings['ChosenCrossingTypeString']!='none')]
+
+cross_per_ped_link = dfPedCrossings.groupby(['run', 'ID', 'CurrentPavementLinkID']).apply(lambda df: df.shape[0])
+assert cross_per_ped_link.loc[ cross_per_ped_link!=1].shape[0]==0
+
+coord_regex = re.compile(r"(\d{6}.\d+)")
+dfPedCrossings['first_coord'] = dfPedCrossings['CrossingCoordsString'].map(lambda s: coord_regex.findall(s.split("),(")[0]))
+dfPedCrossings['geometry'] = dfPedCrossings['first_coord'].map(lambda p:  Point(*map(float, p)))
+
+dfPedCrossingsRun = pd.merge(dfPedCrossings, dfRun, on = 'run', how = 'left')
+
+gdfPedCrossingRun = gpd.GeoDataFrame(dfPedCrossingsRun, geometry = 'geometry')
+for run in gdfPedCrossingRun['run'].unique():
+    gdfSub = gdfPedCrossingRun.loc[ gdfPedCrossingRun['run']==run, ['run','ChosenCrossingTypeString', 'geometry']]
+    gdfSub.to_file("..\\output\\crossing_points_run{}.shp".format(run))
 
 ##########################################
 #
@@ -200,19 +232,56 @@ def batch_runs_road_network_figure(G, dict_node_pos, dfBatch, groupby_columns, f
     f.suptitle(fig_title, fontdict = title_font)
     return f
 
-# Plot crossing counts for each group
-'''
-dfSub = gdfCrossingCounts.loc[ gdfCrossingCounts['run']==1]
-plan_hor = dfSub['tacticalPlanHorizon'].unique()[0]
-min_cross = dfSub['minCrossingProp'].unique()[0]
-title = "Planning horizon:{}, Min cross prop:{}".format(plan_hor, min_cross)
-dict_edge_values = dfSub.set_index('fid')['cmap_value'].to_dict()
-single_fig = road_network_figure(G, dict_node_pos, dict_edge_values, title)
+def batch_runs_crossing_points_figure(G, dict_node_pos, dfBatch, groupby_columns, fig_title, point_size = 3, point_col_dict = {'unmarked':'red','unsignalised':'green'}, point_alpha = 0.5, edge_width = 3, edge_alpha = 1, sub_title_font = {'size': 12}, title_font = {'size': 16}):
+    '''Loop through batch run groups and get edge pallet data for each group. Use this to make road crossings
+    figure for each group.
+    '''
 
-path = os.path.join(img_dir, title.replace(":","_")+".png")
-single_fig.savefig(path)
-single_fig.show()
-'''
+    grouped = dfBatch.groupby(groupby_columns)
+    keys = list(grouped.groups.keys())
+
+    # Want to get separate array of data for each value of
+    p = len(dfBatch[groupby_columns[0]].unique())
+    q = len(dfBatch[groupby_columns[1]].unique())
+
+    key_indices = np.reshape(np.arange(len(keys)), (p,q))
+
+    plt.style.use('dark_background')
+    f, axs = plt.subplots(p, q, figsize = (20,20), sharey=False, sharex=False)
+
+    # Make sure axes array in shame that matches the layout
+    axs = np.reshape(axs, (p, q))
+
+    # Select data to work with and corresponding axis
+    for pi in range(p):
+        for qi in range(q):
+            key_index = key_indices[pi, qi]
+            group_key = keys[key_index]
+            dfRun = grouped.get_group(group_key)
+
+            rename_dict = {'0':'minimise distance', '1':'minimise crossings'}
+            title = "Tactical planning horizon: {}".format(group_key[0])+r"$\degree$"+"\nTactical planning heuristic: {}".format(rename_dict[str(int(group_key[1]))])
+
+            # Select the corresponding axis
+            ax = axs[pi, qi]
+
+            # Draw the road network
+            nx.draw_networkx_nodes(G, dict_node_pos, ax = ax, nodelist=G.nodes(), node_color = 'grey', node_size = 1, alpha = 0.5)
+            nx.draw_networkx_edges(G, dict_node_pos, ax = ax, edgelist=G.edges(), width = 3, edge_color = 'grey', alpha=1)
+            ax.set_title(title, fontdict = title_font)
+            ax.axis('off')
+
+            # Draw the crossing points
+            x = dfRun.loc[ dfRun['ChosenCrossingTypeString']=='unmarked', 'geometry'].map(lambda p:p.x)
+            y = dfRun.loc[ dfRun['ChosenCrossingTypeString']=='unmarked', 'geometry'].map(lambda p:p.y)
+            ax.scatter(x, y, s = point_size, marker = 'o', alpha = point_alpha, c = point_col_dict['unmarked'])
+
+            x = dfRun.loc[ dfRun['ChosenCrossingTypeString']=='unsignalised', 'geometry'].map(lambda p:p.x)
+            y = dfRun.loc[ dfRun['ChosenCrossingTypeString']=='unsignalised', 'geometry'].map(lambda p:p.y)
+            ax.scatter(x, y, s = point_size, marker = 'o', alpha = point_alpha, c = point_col_dict['unsignalised'])
+
+    f.suptitle(fig_title, fontdict = title_font)
+    return f
 
 groupby_columns = ['tacticalPlanHorizon', 'minCrossingProp']
 batch_fig = batch_runs_road_network_figure(G, dict_node_pos, gdfCrossingCounts, groupby_columns, "Crossings per pedestrian on road link", value_col = 'cmap_value', cmap_name = 'viridis', edge_width = 3, edge_alpha = 1)
@@ -227,3 +296,8 @@ um_batch_fig = batch_runs_road_network_figure(G, dict_node_pos, gdfCrossingCount
 um_batch_fig_path = os.path.join(img_dir, "network_figure_informal_"+ os.path.split(ped_crossings_file)[1].replace(".csv", ".png"))
 um_batch_fig.savefig(um_batch_fig_path)
 um_batch_fig.show()
+
+um_batch_fig_path = os.path.join(img_dir, "crossing_points_figure"+ os.path.split(ped_crossings_file)[1].replace(".csv", ".png"))
+points_batch_fig = batch_runs_crossing_points_figure(G, dict_node_pos, dfPedCrossingsRun, groupby_columns, "Crossing Locations", point_size = 3, point_col_dict = {'unmarked':'red', 'unsignalised':'green'}, point_alpha = 0.5, edge_width = 3, edge_alpha = 1, sub_title_font = {'size': 12}, title_font = {'size': 16})
+points_batch_fig.savefig(points_batch_fig_path)
+points_batch_fig.show()
