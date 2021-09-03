8afd455ce00ac6b364001581d413a0b54e30dee2
Obi Thompson Sargoni <33906525+obisargoni@users.noreply.github.com>
2021-09-03 15:31:10 +0100

    One long test of calculating TTC for vehicle-pedestrian conflicts via crossing alternatives.

--- src/repastInterSim/tests/CrossingAlternativeTest.java
+++ src/repastInterSim/tests/CrossingAlternativeTest.java
@@ -3,6 +3,7 @@ package repastInterSim.tests;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,6 +19,8 @@ import repastInterSim.environment.CrossingAlternative;
 import repastInterSim.environment.GISFunctions;
 import repastInterSim.environment.RoadLink;
 import repastInterSim.environment.UnmarkedCrossingAlternative;
+import repastInterSim.environment.Vector;
+import repastInterSim.main.GlobalVars;
 import repastInterSim.main.IO;
 import repastInterSim.main.SpaceBuilder;
 import repastInterSim.pathfinding.TacticalRoute;
@@ -394,4 +397,144 @@ class CrossingAlternativeTest {
 		
 	}
 	
+	@Test
+	void testCATTC() {
+		// Setup the environment
+		try {
+			IO.readProperties();
+			EnvironmentSetup.setUpObjectGeography();
+			EnvironmentSetup.setUpRoads();
+			EnvironmentSetup.setUpPedObstructions();
+
+			EnvironmentSetup.setUpORRoadLinks();
+			EnvironmentSetup.setUpORRoadNetwork(false);
+			
+			EnvironmentSetup.setUpITNRoadLinks();
+			EnvironmentSetup.setUpITNRoadNetwork(true);
+			
+			EnvironmentSetup.setUpPedJunctions();
+			EnvironmentSetup.setUpPavementLinks("pedNetworkLinks.shp");
+			EnvironmentSetup.setUpPavementNetwork();
+						
+			EnvironmentSetup.setUpPedODs();
+			EnvironmentSetup.setUpVehicleODs("mastermap-itn RoadNode Intersect Within.shp");
+			
+			EnvironmentSetup.setUpCrossingAlternatives("crossing_lines.shp");
+			
+			EnvironmentSetup.assocaiteRoadsWithRoadLinks();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// Create pedestrian that will cross first road link
+		Ped ped = EnvironmentSetup.createPedestrian(3,4,false);
+		
+		// Create a vehicle that moves along same link as pedestrian
+		Vehicle v = EnvironmentSetup.createVehicle("osgb4000000029970447", "osgb4000000029970446");
+		try {
+			v.getRoute().setRoute();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		v.setCurrentRoadLinkAndQueuePos(v.getRoute().getRoadsX().get(0)); //  Add vehicle to first road link in its route.
+				
+		// Identify crossing alternatives
+		List<CrossingAlternative> cas = TacticalRoute.getCrossingAlternatives(SpaceBuilder.caGeography, ped.getPathFinder().getStrategicPath().subList(0, 1), ped, SpaceBuilder.roadGeography);
+		
+		// Place ped at edge of a crossing, wlaking in the direction of the other side of the crossing.
+		CrossingAlternative ca = cas.stream().filter(ca_ -> ca_.getType().contentEquals("unsignalised")).collect(Collectors.toList()).get(0);
+		ped.setLoc(ca.getC1());
+		double[] vCross = {ca.getC2().x-ca.getC1().x, ca.getC2().y-ca.getC1().y};
+		ped.setBearing(Vector.angleBetweenNorthAndUnitVector(Vector.unitV(vCross)));
+		
+		// Find mid point
+		Coordinate crossingMid = GISFunctions.midwayBetweenTwoCoordinates(ca.getC1(), ca.getC2());
+		
+		// Point vehicle towards crossing midpoint
+		double[] vectorToMidPoint = {crossingMid.x-v.getLoc().x, crossingMid.y-v.getLoc().y};
+		v.setBearing(Vector.angleBetweenNorthAndUnitVector(Vector.unitV(vectorToMidPoint)));
+		
+		// Get TTC for this ped and this vehicle when ped is stationary
+		HashMap<Vehicle, Double> ttcs = ca.vehicleTTCs(ped);
+		
+		// Because ped is stationary expect null ttc value
+		assert Math.abs(0.0-ped.getSpeed())<0.0000001;
+		assert (ttcs.size() == 1);
+		assert ttcs.values().stream().allMatch(ttc -> ttc==null);
+		
+		// Now set ped speed such that vehicle and ped should collide at the middle of the crossing
+		double vDist = v.getLoc().distance(crossingMid)- GlobalVars.vehicleLength/2;
+		double pDist = ped.getLoc().distance(crossingMid);
+		v.setSpeed(3.0);
+		double tVeh = vDist / v.getSpeed(); // Time it will take front of vehicle to reach crossing.
+		double pedSpeed = pDist / tVeh; // Speed ped should walk at to reach centre of crossing at about the same time as the vehicle.
+		double[] pV = {pedSpeed*Math.sin(ped.getBearing()), pedSpeed*Math.cos(ped.getBearing())}; 
+		ped.setV(pV);
+		
+		ttcs = ca.vehicleTTCs(ped);
+		assert ttcs.size() == 1;
+		assert ttcs.values().stream().allMatch(ttc -> ttc!=null);
+		assert Math.abs(ttcs.get(v) - tVeh) < 0.0000001; 
+		
+		
+		// Test again by placing centre of the vehicle at the crossing midpoint. Make vehicle stationary
+		v.setLoc(crossingMid);
+		v.setSpeed(0.0);
+		v.setBearing(ped.getBearing()+Math.PI/2);
+		tVeh = (pDist - GlobalVars.vehicleWidth/2) / ped.getSpeed();
+		
+		ttcs = ca.vehicleTTCs(ped);
+		assert Math.abs(ttcs.get(v) - tVeh) < 0.0000001;
+		
+		// Test that if vehicle just moves out of the way ttc is null again
+		double vehSpeed = GlobalVars.vehicleLength/2 / tVeh + 0.000001;
+		v.setSpeed(vehSpeed);
+		ttcs = ca.vehicleTTCs(ped);
+		assert ttcs.values().stream().allMatch(ttc -> ttc==null);
+		
+		// Add another vehicle to the road and check that two ttc values are returned
+		Vehicle v2 = EnvironmentSetup.createVehicle("osgb4000000029970447", "osgb4000000029970446");
+		try {
+			v2.getRoute().setRoute();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		v2.setCurrentRoadLinkAndQueuePos(v2.getRoute().getRoadsX().get(0)); //  Add vehicle to first road link in its route.
+		
+		ttcs = ca.vehicleTTCs(ped);
+		assert ttcs.size()==2;
+		assert ttcs.values().stream().allMatch(ttc -> ttc==null);
+		
+		// Move ped and test getting ttc via unmarked crossing alternative
+		UnmarkedCrossingAlternative uca = (UnmarkedCrossingAlternative) cas.stream().filter(ca_ -> ca_.getType().contentEquals("unmarked")).collect(Collectors.toList()).get(0);
+		
+		ped.setLoc(new Coordinate(530451, 180855));
+		double[] vCross2 = {uca.getC2().x-uca.getC1().x, uca.getC2().y-uca.getC1().y};
+		ped.setBearing(Vector.angleBetweenNorthAndUnitVector(Vector.unitV(vCross2)));
+		crossingMid = GISFunctions.midwayBetweenTwoCoordinates(uca.getC1(), uca.getC2());
+		
+		// Point vehicle towards crossing midpoint
+		double[] vectorToMidPoint2 = {crossingMid.x-v2.getLoc().x, crossingMid.y-v2.getLoc().y};
+		v2.setBearing(Vector.angleBetweenNorthAndUnitVector(Vector.unitV(vectorToMidPoint2)));
+		
+		vDist = v2.getLoc().distance(crossingMid)- GlobalVars.vehicleLength/2;
+		pDist = ped.getLoc().distance(crossingMid);
+		v2.setSpeed(3.0);
+		tVeh = vDist / v2.getSpeed(); // Time it will take front of vehicle to reach crossing.
+		pedSpeed = pDist / tVeh; // Speed ped should walk at to reach centre of crossing at about the same time as the vehicle.
+		double[] pV2 = {pedSpeed*Math.sin(ped.getBearing()), pedSpeed*Math.cos(ped.getBearing())}; 
+		ped.setV(pV2);
+		
+		ttcs = uca.vehicleTTCs(ped);
+		assert ttcs.size()==2;
+		assert ttcs.get(v) == null;
+		assert Math.abs(ttcs.get(v2)-tVeh)<0.00000001;
+		
+	}
 }
