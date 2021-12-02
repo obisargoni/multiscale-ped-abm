f838c9675665b864deee4eaf7e8f8394903302e9
Obi Sargoni <othompsarg@hotmail.com>
2021-12-02 11:28:30 +0000

    Calculating route lengths

--- scripts/compare_routes.py
+++ scripts/compare_routes.py
@@ -712,6 +712,7 @@ batch_file = bd_utils.most_recent_directory_file(data_dir, file_re)
 
 # output paths for processed data
 output_vehicle_density_file = os.path.join(data_dir, "av_vehicle_density.{}.csv".format(vehicle_density_timestamp))
+output_route_length_file = os.path.join(data_dir, "run_route_length.{}.csv".format(vehicle_density_timestamp))
 output_sp_similarity_path = os.path.join(data_dir, "sp_similarity.{}.csv".format(file_datetime_string))
 output_sp_similarity_length_path = os.path.join(data_dir, "path_length_sp_similarity.{}.csv".format(file_datetime_string))
 output_route_completion_path = os.path.join(data_dir, "route_completions.{}.csv".format(file_datetime_string))
@@ -777,6 +778,8 @@ dfLinkCrossCounts = get_road_link_pedestrian_crossing_counts(dfCrossEvents, gdfP
 
 # Data aggregated to run level, used to calculate sensitivity indices
 dfRouteCompletion = agg_route_completions(dfPedRoutes, dfRun, output_path = output_route_completion_path)
+
+dfRouteLength = get_run_total_route_length(dfPedRoutes, dfRun, pavement_graph, exclude_stuck_peds = True, output_path = "run_route_length.csv")
 dfSPSim = get_shortest_path_similarity(dfPedRoutes, dfRun, pavement_graph, dict_node_pos, weight_params, distance_function = 'dice_dist', exclude_stuck_peds = True, output_path = output_sp_similarity_path)
 dfSPSimLen = get_shortest_path_similarity(dfPedRoutes, dfRun, pavement_graph, dict_node_pos, weight_params, distance_function = 'path_length', exclude_stuck_peds = True, output_path = output_sp_similarity_length_path)
 dfConflicts = agg_cross_conflicts(dfCrossEvents, dfLinkCrossCounts, ttc_col = 'TTC')
