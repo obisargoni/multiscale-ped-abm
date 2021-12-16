d0b6c104f7e07b74cd112947e68bbec0c0129435
Obi Sargoni <othompsarg@hotmail.com>
2021-12-16 14:51:37 +0000

    New class for recording crossing events data

---
+++ src/repastInterSim/datasources/CrossEventData.java
@@ -0,0 +1,59 @@
+package repastInterSim.datasources;
+
+import java.util.List;
+
+import com.vividsolutions.jts.geom.Coordinate;
+
+public class CrossEventData {
+	
+	private int pedID;
+	private String tacticalEdgeID;
+	private String crossingType;
+	private String crossingCoordinatesString;
+	private Double ttc;
+
+	public CrossEventData(int pID, String tEID, String cT, List<Coordinate> cCs) {
+		this.pedID=pID;
+		this.tacticalEdgeID=tEID;
+		this.crossingType=cT;
+		
+    	String ccString = "";
+    	for (Coordinate c: cCs) {
+    		ccString += c.toString();
+    		ccString += ",";
+    	}
+		this.crossingCoordinatesString=ccString;
+		
+		this.ttc=null;
+	}
+
+	public int getPedID() {
+		return pedID;
+	}
+
+	public String getTacticalEdgeID() {
+		return tacticalEdgeID;
+	}
+
+	public String getCrossingType() {
+		return crossingType;
+	}
+
+	public String getCrossingCoordinatesString() {
+		return crossingCoordinatesString;
+	}
+
+	public String getTTCString() {
+    	if (this.ttc==null) {
+    		return "";
+    	}
+    	else {
+    		return this.ttc.toString();
+    	}
+	}
+
+	public void setTTC(Double ttc) {
+		this.ttc = ttc;
+	}
+
+}
