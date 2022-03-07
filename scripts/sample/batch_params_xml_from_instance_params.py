2eec1e25991ad2b04afa4e837e6e3afc29b0a465
Obi Sargoni <othompsarg@hotmail.com>
2022-03-07 17:18:33 +0000

    Making script easier to adjust so that instance runs of interest can be identified and re-run

--- scripts/sample/batch_params_xml_from_instance_params.py
+++ scripts/sample/batch_params_xml_from_instance_params.py
@@ -1,24 +1,34 @@
 import pandas as pd
+import re
+import os
 from SALibRepastParams import *
 
-dfBatchParams = pd.read_csv("param_input.txt", delimiter=",", header=None)
-raw_cols = dfBatchParams.loc[0]
-cols = ["".join(i.split("\t")[:-1]) for i in raw_cols]
-cols[0] = cols[0].replace("1","")
-dfBatchParams.columns = cols
+directory = "C:\\Users\\mecha_user\\AppData\\Local\\Temp\\3\\simphony_model_1645025809358\\"
+instances = ["instance_15", "instance_17", "instance_28"]
 
-for i in cols:
-    dfBatchParams[i] = dfBatchParams[i].map(lambda x: x.split("\t")[-1])
+dfInstRuns = pd.DataFrame()
+for inst in instances:
+    f = os.path.join(directory, inst, "param_input.txt")
+    df = pd.read_csv(f, header=None)
+    raw_cols = df.loc[0]
+    cols = ["".join(i.split("\t")[:-1]) for i in raw_cols]
+    cols[0] = re.search(r"(\D+)", cols[0]).groups()[0]
+    df.columns = cols
 
-    
-# All stuck around the same types of params
-dfBatchParams['run'] = list(range(1,dfBatchParams.shape[0]+1))
+    df['run'] = df.iloc[:, 0].map(lambda x: x.split("\t")[0])
+    for i in cols:
+        if i == "run":
+            continue
+        df[i] = df[i].map(lambda x: x.split("\t")[-1])
+
+    df['inst'] = inst
+    dfInstRuns = pd.concat([dfInstRuns, df])
 
 problem = init_problem(params = params)
 repast_params = copy.deepcopy(params)
 
-dfBatchParams['minCrossing'] = dfBatchParams['minCrossing'].astype(int)
-sampled_values = dfBatchParams.loc[:, problem['names']].values
+dfInstRuns['minCrossing'] = dfInstRuns['minCrossing'].map(lambda x: True if x.lower()=='true' else False).astype(int)
+sampled_values = dfInstRuns.loc[:, problem['names']].values
 
 for i, name in enumerate(problem['names']):
     param_values = sampled_values[:, i]
@@ -28,7 +38,8 @@ for i, name in enumerate(problem['names']):
     if repast_params[name]['data_type']=='int':
         param_values = param_values.astype(int)
     elif repast_params[name]['data_type']=='boolean':
-        param_values = np.round(param_values).astype(bool)
+        #param_values = np.round(param_values).astype(bool)
+        param_values = param_values.astype(bool)
 
     repast_params[name]['values'] = " ".join(str(v).lower() for v in param_values)
 
